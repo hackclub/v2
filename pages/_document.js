2c3e6c670b111ed8e385b98afc21b78544a61dd0
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-09-23 23:26:54 -0400

    Add Next page

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    )
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Hack Club</title>
+          <style
+            children={`*{box-sizing:border-box}body{line-height:1.6;margin:0}`}
+          />
+          <link rel="stylesheet" href="fakt-soft.css" />
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import App from '../src/App'
+
+export default () => <App />

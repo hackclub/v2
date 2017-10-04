3fa00af0c3ce5dcdbda30b1ec94e6cfeee4ca432
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-03 20:04:04 -0400

    Iterating on manifesto design

--- pages/about.js
+++ pages/about.js
@@ -24,28 +24,43 @@ import {
 } from 'rebass'
 import theme from '../src/theme'
 import Flag from '../src/Flag'
+import Footer from '../src/Footer'
+
+const FONT = "SFMono-Regular, 'Roboto Mono', Menlo, 'Courier New', monospace"
 
 export default () => (
   <Provider theme={theme}>
     <Flag />
-    <Container mt={5} py={4} px={3}>
-      <Heading color="primary">A manifesto</Heading>
-      <Text my={2}>
+    <Container mt={5} py={4} px={3} maxWidth={36 * 16}>
+      <Heading color="primary" f={6}>
+        Our manifesto.
+      </Heading>
+      <p>
         Bits are replacing atoms, algorithms are attaining agency, and
         "infrastructure" is coming to mean cloud services, not roads and
         railways. Within the next few years, algorithms will be driving our cars
         and defining our virtual worlds.
-      </Text>
-      <Text my={2}>
+      </p>
+      <p>
         Yet while the impact of technology is increasing, we are suffering a
         crisis of opportunity: half American high schools don't offer computer
         science courses and the ones that do fail to get beginners excited.
-      </Text>
-      <Text my={2}>
-        We need to change our approach and put students in charge. And that's
-        what Hack Club is, students starting the computer science classes their
-        schools should offer.
-      </Text>
+      </p>
+      <p>
+        We need to change our approach and put students in charge.{' '}
+        <strong>
+          And that's what Hack Club is, students starting the computer science
+          classes their schools should offer.
+        </strong>
+      </p>
+      <style jsx>{`
+        p {
+          font-family: ${FONT};
+          font-size: 1.1rem;
+          margin-bottom: 1rem;
+        }
+      `}</style>
     </Container>
+    <Footer />
   </Provider>
 )

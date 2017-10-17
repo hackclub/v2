ea2ee92f651d19a528ed3142c3402d4dfcc7012b
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-17 10:16:28 -0400

    Restyle About page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,57 +1,71 @@
 import React from 'react'
 import {
   Provider,
-  Banner,
   Heading,
-  Badge,
   Lead,
   Container,
   Flex,
   Box,
   Text,
-  Subhead,
-  Button,
-  ButtonOutline,
-  Label,
-  Input,
-  Circle,
-  Relative,
-  Absolute,
-  Image,
-  Link,
-  Row,
-  Column
+  Subhead
 } from 'rebass'
-import theme from '../theme'
+import theme, { colors } from '../theme'
 import Flag from '../components/Flag'
 import Footer from '../components/Footer'
+
+const Base = Container.extend.attrs({
+  mt: 5,
+  py: 4,
+  px: 3,
+  maxWidth: 36 * 16
+})``
+
+const Headline = Heading.extend.attrs({
+  color: 'primary',
+  mb: 3,
+  align: 'center'
+})`
+  text-transform: uppercase;
+  letter-spacing: .1em;
+`
+
+const P = Text.extend.attrs({ f: 3, mb: 2 })`
+  &:first-of-type:first-letter {
+    initial-letter: 2;
+    -webkit-initial-letter: 2;
+    color: ${colors.slate};
+    font-weight: bold;
+    margin-right: .5rem;
+  }
+`
 
 export default () => (
   <Provider theme={theme}>
     <Flag />
-    <Container mt={5} py={4} px={3} maxWidth={36 * 16}>
-      <Heading color="primary" f={6} mb={3}>
-        Our manifesto.
-      </Heading>
-      <Text f={3} mb={2}>
+    <Base>
+      <Headline>
+        <Box f={4}>The Hack Club</Box>
+        <Box f={6}>Manifesto</Box>
+      </Headline>
+      <P>
         Bits are replacing atoms, algorithms are attaining agency, and
         "infrastructure" is coming to mean cloud services, not roads and
         railways. Within the next few years, algorithms will be driving our cars
         and defining our virtual worlds.
-      </Text>
-      <Text f={3} mb={2}>
+      </P>
+      <P>
         Yet while the impact of technology is increasing, we are suffering a
         crisis of opportunity: half American high schools don't offer computer
         science courses and the ones that do fail to get beginners excited.
-      </Text>
-      <Text f={3} mb={2}>
+      </P>
+      <P>
         We need to change our approach and put students in charge.{' '}
         <strong>
           And that's what Hack Club is, students starting the computer science
           classes their schools should offer.
         </strong>
-      </Text>
-    </Container>
+      </P>
+    </Base>
     <Footer />
   </Provider>
 )

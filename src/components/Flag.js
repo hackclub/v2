bf185e0938ea5311c412d910c40773105b83485d
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-17 00:28:13 -0400

    Link Orpheus flag home

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Absolute } from 'rebass'
+import styled from 'styled-components'
+import { Link } from 'react-static'
 import { mx } from '../theme'
 
-const Flag = Absolute.extend.attrs({ top: true, left: true, right: true })`
+const Flag = styled(Link).attrs({ to: '/' })`
   background: url(/orpheus_flag.svg) center no-repeat;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
   height: 5rem;
   z-index: 0;
 

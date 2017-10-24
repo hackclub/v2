f8ce72b7efedba381333c2a329536117d6e430cd
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-23 21:32:02 -0400

    Segment analytics

--- static.config.js
+++ static.config.js
@@ -37,6 +37,7 @@ export default {
       const newChildren = sheet.collectStyles(children)
       const styleTags = sheet.getStyleElement()
 
+      const segment = `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0"; analytics.load("8SLCkrqC517pTQdMvqP1eSP8FkH8yFzE"); analytics.page(); }}();`
       const css = `
         *{box-sizing:border-box}body{line-height:1.6;margin:0}
 
@@ -76,6 +77,7 @@ export default {
               content="width=device-width, initial-scale=1"
             />
             <meta name="format-detection" content="telephone=no" />
+            <script dangerouslySetInnerHTML={{ __html: segment }} />
             <style dangerouslySetInnerHTML={{ __html: css }} />
             {styleTags}
           </Head>

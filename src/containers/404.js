9e335d29f7d5d1d2e974853e2cb7f0d010f2f0df
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-25 21:30:10 -0400

    Basic new 404 page

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,8 +1,20 @@
 import React from 'react'
-//
+import { Provider, Heading, Container, Box, Text } from 'rebass'
+import theme from '../theme'
+import { Head, Link } from 'react-static'
+import Flag from '../components/Flag'
+import CTA from '../components/CTA'
+import Footer from '../components/Footer'
 
 export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
-  </div>
+  <Provider theme={theme}>
+    <Head><title>404 – Hack Club</title></Head>
+    <Flag />
+    <Container maxWidth={48 * 16} mt={4} px={3} py={5} align="center">
+      <Heading color="primary" f={[5, 6]}>404!</Heading>
+      <Text f={4} mt={2} mb={3} color="muted">We couldn’t find that page.</Text>
+      <CTA is={Link} to="/" bg="primary" color="white">Go Home</CTA>
+    </Container>
+    <Footer />
+  </Provider>
 )

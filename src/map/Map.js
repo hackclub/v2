ccca8d389b1c2c7b2a4ed463860c25eca6a7d197
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-29 15:15:37 -0400

    Render map programatically

--- src/map/Map.js
+++ src/map/Map.js
@@ -7,39 +7,28 @@ const {
   Markers,
   Marker
 } = require('react-simple-maps')
-const { colors, mm } = require('../theme')
 const locations = require('./locations')
-const geo = require('./geo')
-const styled = require('styled-components').default
 
-const Geo = styled(Geography)`
-  fill: ${colors.blue[0]};
-  stroke: ${colors.snow};
-  stroke-width: 1;
-  outline: none;
-  &[d^='M111.4352835706319'] { display: none; } /* hide antartica */
-`
-
-const Pin = styled.circle.attrs({ cx: 0, cy: 0, r: 8 })`
-  fill: ${colors.primary};
-  stroke-width: 0;
-  opacity: ${2 / 3};
-`
-
-export default props => (
+const Map = ({ paths, ...props }) => (
   <ComposableMap width={768} {...props}>
     <ZoomableGroup disablePanning>
-      <Geographies geographyUrl="https://unpkg.com/world-atlas@1.1.4/world/50m.json">
+      <Geographies geographyPaths={paths}>
         {(geographies, projection) =>
           geographies.map((geography, i) => (
-            <Geo key={i} geography={geography} projection={projection} />
+            <Geography key={i} geography={geography} projection={projection} />
           ))}
       </Geographies>
       <Markers>
         {locations.map((marker, i) => (
-          <Marker key={i} marker={marker} children={<Pin />} />
+          <Marker
+            key={i}
+            marker={marker}
+            children={<circle cx={0} cy={0} r={6} />}
+          />
         ))}
       </Markers>
     </ZoomableGroup>
   </ComposableMap>
 )
+
+export default Map

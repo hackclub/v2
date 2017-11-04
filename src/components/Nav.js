1793db246ba7869c58993a67393de091e683eeeb
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-11-04 17:23:41 -0400

    Add cloud prop to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Box, Flex, Image } from 'rebass'
 import { Link } from 'react-static'
-import { mx } from '../theme'
+import { colors, mx } from '../theme'
 
 const Base = Flex.extend.attrs({
   px: 4,
-  py: 2,
+  py: 1,
   justify: 'space-between',
   w: 1
 })`
@@ -16,23 +16,31 @@ const Logo = Image.extend.attrs({ w: 48 })`
   border-radius: 2rem;
 `
 
-const Nav = Flex.extend.attrs({ is: 'nav', align: 'center', mx: -3 })`
-  background-color: rgba(255,255,255,.98);
-  box-shadow: 0 0 1rem 1rem rgba(255,255,255,.98);
-  border-radius: 2rem;
-`
+const Nav = Flex.extend.attrs({ is: 'nav', align: 'center', mx: -3 })(
+  [],
+  props =>
+    props.cloud
+      ? {
+          backgroundColor: 'rgba(255,255,255,.98)',
+          boxShadow: '0 0 1rem 1rem rgba(255,255,255,.98)',
+          borderRadius: '2rem',
+          color: colors.slate
+        }
+      : { color: colors.white }
+)
 
-const Item = Box.extend.attrs({ mx: 3, color: 'slate' })`
+const Item = Box.extend.attrs({ mx: 3 })`
+  color: inherit;
   text-decoration: none;
   font-weight: bold;
 `
 
-export default props => (
+export default ({ cloud, ...props }) => (
   <Base {...props}>
     <Link to="/">
       <Logo src="/logo-nav.png" alt="Hack Club logo" />
     </Link>
-    <Nav>
+    <Nav cloud={cloud}>
       <Item is="a" href="/workshops" children="Workshops" />
       <Item is={Link} to="/team" children="Our Team" />
     </Nav>

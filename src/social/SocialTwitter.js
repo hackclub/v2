6b2ab3e139c78be585169fc2ee5f484973fc8074
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-12-09 22:14:06 -0500

    New Twitter avatar

---
+++ src/social/SocialTwitter.js
@@ -0,0 +1,30 @@
+const { createElement: h } = require('react')
+const theme = require('../theme').default
+
+const width = 1024
+const widthIcon = 2 * width / 3
+
+module.exports = props =>
+  h(
+    'main',
+    {
+      style: {
+        boxSizing: 'border-box',
+        width,
+        height: width,
+        paddingLeft: (width - widthIcon) / 2 + 16,
+        background: `${theme.colors.primary}
+          url(http://0.0.0.0:3000/pattern.svg) repeat`,
+        backgroundSize: width
+      }
+    },
+    h('div', {
+      style: {
+        width: widthIcon,
+        height: width,
+        background: 'url(http://0.0.0.0:3000/logo-stacked.svg) no-repeat',
+        backgroundSize: '100% auto',
+        backgroundPosition: 'center'
+      }
+    })
+  )

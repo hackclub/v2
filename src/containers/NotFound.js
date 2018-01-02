380b3ebbb8a170d94f2e5f46ee67b1c27108bd7f
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-02 13:50:02 -0500

    Convert NotFound page

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import { Provider, Heading, Container, Box, Text } from 'rebass'
-import theme from '../theme'
-import { Head, Link } from 'react-static'
+import {
+  ThemeProvider,
+  Heading,
+  Container,
+  LargeButton,
+  Text
+} from '@hackclub/design-system'
+import { Head } from 'react-static'
 import Nav from '../components/Nav'
-import CTA from '../components/CTA'
 import Footer from '../components/Footer'
 
 export default () => (
-  <Provider theme={theme}>
-    <Head><title>Hack Club</title></Head>
-    <Nav />
-    <Container maxWidth={48 * 16} px={3} py={6} align="center">
-      <Heading color="primary" f={[5, 6]}>404!</Heading>
-      <Text f={4} mt={2} mb={3} color="muted">We couldn’t find that page.</Text>
-      <CTA is={Link} to="/" bg="primary" color="white">Go Home</CTA>
+  <ThemeProvider>
+    <Head>
+      <title>Hack Club</title>
+    </Head>
+    <Nav color="primary" />
+    <Container maxWidth={48} px={2} py={6} align="center">
+      <Heading.h1 color="primary" f={[5, 6]}>
+        404!
+      </Heading.h1>
+      <Text f={4} mt={2} mb={3} color="muted">
+        We couldn’t find that page.
+      </Text>
+      <LargeButton href="/">Go Home 🏡</LargeButton>
     </Container>
     <Footer />
-  </Provider>
+  </ThemeProvider>
 )

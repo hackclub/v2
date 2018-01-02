74dd40ac7592eee1eb6b59ce06c44291bd54e644
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-02 03:33:36 -0500

    Convert Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,72 +1,78 @@
 import React from 'react'
-import { Subhead, Container, Flex, Box, Text, Row, Column } from 'rebass'
+import { Flex, Box, Heading, Text, Link as A } from '@hackclub/design-system'
 import { Link } from 'react-static'
 import Icon from './Icon'
-import { mx, geo, grays } from '../theme'
+import { mx, geo } from '../theme'
 
-const Base = Box.extend.attrs({ is: 'footer', py: 5, px: 2 })`
-  ${geo(grays.snow)};
+const Base = Flex.withComponent('footer').extend`
+  ${props => geo(props.theme.colors.snow)};
+  > div { max-width: 64rem; }
+  div { flex: 1 1 auto; }
 `
 
-const LeftCol = Column.extend.attrs({ w: [1, 0.4], mb: 2 })`
-  ${mx[1]} {
-    text-align: right;
-  }
-`
-const RightCol = Column.extend.attrs({ w: [1, 0.6] })`
-  text-align: left;
-`
+const LeftCol = Box.withComponent('aside').extend.attrs({
+  px: 3,
+  mb: 2,
+  w: [1, 0.6],
+  align: ['left', 'right']
+})``
+const RightCol = Box.withComponent('article').extend.attrs({
+  px: 3,
+  w: [1, 0.4]
+})``
 
-const Service = ({ href, icon, fill = 'muted', size = 32, ...props }) => (
-  <Box is="a" target="_blank" href={href} mx={2} {...props}>
-    <Icon name={icon} size={size} fill={fill} />
-  </Box>
+const Service = ({ href, icon, ...props }) => (
+  <A target="_blank" href={href} mx={2} title={icon} {...props}>
+    <Icon name={icon} size={32} fill="muted" alt={icon} />
+  </A>
 )
 
 const Footer = () => (
-  <Base>
-    <Container>
-      <Row w={1} wrap>
-        <LeftCol>
-          <Subhead>Learn like a hacker.</Subhead>
-        </LeftCol>
-        <RightCol>
-          <Flex align="center" mx={-2}>
-            <Service href="/slack_invite" icon="slack" />
-            <Service href="https://twitter.com/starthackclub" icon="twitter" />
-            <Service href="https://github.com/hackclub" icon="github" />
-            <Service
-              href="https://www.instagram.com/starthackclub"
-              icon="instagram"
-            />
-            <Service
-              href="https://www.facebook.com/Hack-Club-741805665870458"
-              icon="facebook"
-            />
-            <Service href="mailto:team@hackclub.com" icon="mail_outline" />
-          </Flex>
-        </RightCol>
-      </Row>
-      <Row w={1} wrap>
-        <LeftCol>
-          <Subhead>Hack Club</Subhead>
-        </LeftCol>
-        <RightCol>
-          <Text>
-            576 Natoma St<br />San Francisco, CA 94103
-          </Text>
-          <Text mt={2}>Nonprofit EIN: 81-2908499</Text>
-          <Text mt={2}>
-            <Text is="a" href="https://conduct.hackclub.com" color="info">
-              Read our Code of Conduct
-            </Text>
-          </Text>
-          <Text f={1} color="muted" mt={3}>
-            © 2017 Hack Club
-          </Text>
-        </RightCol>
-      </Row>
-    </Container>
+  <Base flexDirection="column" alignItems="center" p={[4, 5]}>
+    <Flex w={1} mx={-3} wrap>
+      <LeftCol>
+        <Heading.h3 bold m={0}>
+          Learn like a hacker.
+        </Heading.h3>
+      </LeftCol>
+      <RightCol>
+        <Flex align="center" mx={-2}>
+          <Service href="/slack_invite" icon="slack" />
+          <Service href="https://twitter.com/starthackclub" icon="twitter" />
+          <Service href="https://github.com/hackclub" icon="github" />
+          <Service
+            href="https://www.instagram.com/starthackclub"
+            icon="instagram"
+          />
+          <Service
+            href="https://www.facebook.com/Hack-Club-741805665870458"
+            icon="facebook"
+          />
+          <Service href="mailto:team@hackclub.com" icon="mail_outline" />
+        </Flex>
+      </RightCol>
+    </Flex>
+    <Flex w={1} mx={-3} mt={3} wrap>
+      <LeftCol>
+        <Heading.h3 bold m={0}>
+          Hack Club HQ
+        </Heading.h3>
+      </LeftCol>
+      <RightCol>
+        <Text m={0}>
+          576 Natoma St<br />San Francisco, CA 94103
+        </Text>
+        <Text>Nonprofit EIN: 81-2908499</Text>
+        <Text>
+          <A href="https://conduct.hackclub.com" color="info" underline>
+            Read our Code of Conduct
+          </A>
+        </Text>
+        <Text f={1} color="muted" mt={3}>
+          © 2017 Hack Club
+        </Text>
+      </RightCol>
+    </Flex>
   </Base>
 )
 

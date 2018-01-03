01fe3819368a612c791ede861bfebcb4430606ac
Max Wofford <max@maxwofford.com>
2018-01-03 01:20:08 -0800

    Add a loading animation

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -1,5 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Flex, Text } from 'rebass'
 
-export default () => (
-  <p>Loading...</p>
-)
+const Base = Flex.extend`
+justify-content: center;
+align-items: center;
+position: absolute;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+`
+
+export default class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {dots: 1}
+  }
+
+  componentDidMount() {
+    this.id = setInterval(() => {
+      const {dots} = this.state
+      this.setState({
+        dots: (dots % 3) + 1
+      })
+    }, 300)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.id)
+  }
+
+  render() {
+    return (
+      <Base>
+        <Text>Loading{'.'.repeat(this.state.dots)}</Text>
+      </Base>
+    )
+  }
+}

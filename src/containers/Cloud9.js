15c2603d014835b1591480026fa4c715eeb0001e
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-03 23:50:12 -0500

    Add Cloud9 form

---
+++ src/containers/Cloud9.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { ThemeProvider, Box, Heading, Section } from '@hackclub/design-system'
+import { Head } from 'react-static'
+import Nav from '../components/Nav'
+import Cloud9Form from '../components/Cloud9Form'
+
+const Header = Section.withComponent('header').extend`
+  flex-direction: column;
+  padding-top: 0 !important;
+  background-color: ${props => props.theme.colors.blue[6]};
+  background-image: linear-gradient(
+    -8deg,
+    ${props => props.theme.colors.indigo[4]} 0%,
+    ${props => props.theme.colors.blue[6]} 50%,
+    ${props => props.theme.colors.blue[7]} 100%
+  );
+`
+
+export default () => (
+  <ThemeProvider>
+    <Head>
+      <title>Cloud9 – Hack Club</title>
+    </Head>
+    <Header pb={4} align="center">
+      <Nav />
+      <Heading.h1 f={[5, 6]} mt={4} mb={2}>
+        Let’s get you coding on Cloud9.
+      </Heading.h1>
+      <Heading f={[3, 4]}>
+        Enter your email below and you’ll receive an invitation.
+      </Heading>
+    </Header>
+    <Cloud9Form />
+  </ThemeProvider>
+)
--- static.config.js
+++ static.config.js
@@ -48,6 +48,10 @@ export default {
       component: 'src/containers/ApplyRepl'
     },
     {
+      path: '/cloud9_setup',
+      component: 'src/containers/Cloud9'
+    },
+    {
       path: '/redeem_tech_domain',
       component: 'src/containers/RedeemTechDomain'
     },

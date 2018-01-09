ab6b22f1609be45b70063e7b901af42f4139070d
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-09 00:40:13 -0500

    Update Camp page

--- src/containers/Camp.js
+++ src/containers/Camp.js
@@ -3,68 +3,61 @@ import {
   Button,
   Container,
   Heading,
-  Link,
-  Section,
   Text,
+  Section,
+  Link as A,
   ThemeProvider
 } from '@hackclub/design-system'
-import { cx } from '../theme'
-import { Head } from 'react-static'
+import { Head, Link } from 'react-static'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 
 // These 2 colors are from the gradient used on the original Hack Camp website
-const oldCampTheme = ['rgb(255, 75, 85)', 'rgb(212, 78, 116)']
+const campTheme = ['rgb(255, 75, 85)', 'rgb(212, 78, 116)']
 
 const Header = Section.withComponent('header').extend`
   flex-direction: column;
   padding-top: 0 !important;
-  background-color: ${cx('campPrimary')};
+  background-color: ${campTheme[0]};
   background-image: linear-gradient(
     45deg,
-    ${cx('campPrimary')} 0%,
-    ${cx('campSecondary')} 100%
+    ${campTheme[0]} 0%,
+    ${campTheme[1]} 100%
   );
 `
 
-const Detail = Section.extend`
-  flex-direction: column;
-  color: black;
-`
+Button.link = Button.withComponent(Link)
 
 export default () => (
   <ThemeProvider>
     <Head>
       <title>Hack Camp</title>
     </Head>
-    <Header pb={4}>
+    <Header pb={3}>
       <Nav />
-      <Heading.h1 f={[5, 6]} mt={[4, 5]}>
+      <Heading.h1 f={[5, 6]} mt={4}>
         Hack Camp
       </Heading.h1>
-      <Heading f={[3, 4]} my={2}>
+      <Heading f={[3, 4]} mt={2}>
         We’ve replaced camps in favor of clubs!
       </Heading>
     </Header>
-    <Detail>
-      <Container>
-        <Text f={2}>
-          Apply to our clubs program and we’ll help you launch and lead a coding
-          club at your school. It’s like summer camp, but it runs all year!
-        </Text>
-        <Button.link bg={cx('campPrimary')} color="white" to="/start" my={4}>
-          Learn More And Apply »
-        </Button.link>
-        <Text color="slate">
-          Looking for the old Hack Camp website? You can find it{' '}
-          <Link
-            color={cx('campPrimary')}
-            href="https://hackclub.github.io/camp/"
-          >
-            here
-          </Link>.
-        </Text>
-      </Container>
-    </Detail>
+    <Container maxWidth={32} py={[3, 4]} align="center">
+      <Text f={3}>
+        Apply to our clubs program and we’ll help you launch and lead a coding
+        club at your school. It’s like summer camp, but it runs all year!
+      </Text>
+      <Button.link bg={campTheme[0]} color="white" to="/start" my={4}>
+        Learn More + Apply »
+      </Button.link>
+      <Text color="slate">
+        Looking for the{' '}
+        <A
+          color={campTheme[0]}
+          href="https://hackclub.github.io/camp/"
+          children="old Hack Camp website"
+        />?
+      </Text>
+    </Container>
   </ThemeProvider>
 )

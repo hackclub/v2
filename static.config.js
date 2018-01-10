0ab6b02b82ad54ed471b1f16a59d48e2bc111c4b
Victor Truong <me@ifvictr.com>
2018-01-10 07:31:40 +0000

    Create basic contributors page

--- static.config.js
+++ static.config.js
@@ -60,6 +60,10 @@ export default {
       component: 'src/containers/Camp'
     },
     {
+      path: '/contributors',
+      component: 'src/containers/Contributors'
+    },
+    {
       is404: true,
       component: 'src/containers/NotFound'
     }

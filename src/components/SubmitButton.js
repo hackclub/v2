5beb7c0bef3ca5ba04587492d10b79d06c23fef5
Max Wofford <max@maxwofford.com>
2018-01-21 19:29:06 -0800

    Submit button style changes

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -3,40 +3,49 @@ import api from '../api'
 import { LargeButton } from '@hackclub/design-system'
 import { clubApplicationSchema } from './ClubApplicationForm'
 
-
 export default class extends Component {
   constructor(props) {
     super(props)
 
     this.state = { readyToSubmit: false, submitted: false }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentWillMount() {
-    const { authToken, application } = this.props
+    const { application } = this.props
+    const allProfilesFinished = !application.applicant_profiles.some(
+      p => p.completed_at === null
+    )
+    const validApp = clubApplicationSchema.isValidSync(application)
+    const unsubmittedApp = application.submitted_at === null
 
-    let readyToSubmit = clubApplicationSchema.isValidSync(application)
-
-    if (readyToSubmit) {
-      application.applicant_profiles.forEach(profile => {
-        if (profile.completed_at === null) {
-          readyToSubmit = false
-        }
+    if (allProfilesFinished && validApp && unsubmittedApp) {
+      this.setState({
+        readyToSubmit: true
       })
     }
 
     if (application.submitted_at) {
-      readyToSubmit = false
-      this.setState({submitted: true})
+      this.setState({
+        submitted: true
+      })
     }
-
-    this.setState({readyToSubmit})
   }
 
   handleSubmit() {
+    const { authToken, application } = this.props
+    const { readyToSubmit, submitted } = this.state
+
+    if (submitted || !readyToSubmit) {
+      return null
+    }
+
     api
       .post(`v1/new_club_applications/${application.id}/submit`, { authToken })
       .then(() => {
         alert('Your application has been submitted!')
+        this.setState({ submitted: true })
       })
       .catch(e => {
         alert(e.statusText)
@@ -45,9 +54,19 @@ export default class extends Component {
 
   render() {
     const { readyToSubmit, submitted } = this.state
+
     return (
-      <LargeButton onClick={this.handleSubmit} w={1} mb={2} inverted bg={readyToSubmit ? "success" : "primary"} disabled={!readyToSubmit}>
-        {submitted ? 'We’ve recieved your application' : 'Submit your application'}
+      <LargeButton
+        onClick={this.handleSubmit}
+        bg={readyToSubmit ? 'success' : 'primary'}
+        disabled={submitted || !readyToSubmit}
+        w={1}
+        mb={2}
+        inverted={!readyToSubmit}
+      >
+        {submitted
+          ? 'We’ve recieved your application'
+          : 'Submit your application'}
       </LargeButton>
     )
   }

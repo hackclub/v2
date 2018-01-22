2382d2931aa59a65fda6e2cdf2b39f2fab015cfc
Max Wofford <max@maxwofford.com>
2018-01-22 03:04:11 -0800

    Remove unnecessary import

--- src/components/LeaderApplicationForm.js
+++ src/components/LeaderApplicationForm.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { api } from '../../data'
 import { Container } from '@hackclub/design-system'
 import {

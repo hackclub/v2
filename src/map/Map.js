98db6b9078b101a63a03eb34d2a6b1b279730f07
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-22 23:55:02 -0500

    Bring back map

---
+++ src/map/Map.js
@@ -0,0 +1,34 @@
+const React = require('react')
+const {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+  Markers,
+  Marker
+} = require('react-simple-maps')
+
+const Map = ({ paths, locations, ...props }) => (
+  <ComposableMap width={768} {...props}>
+    <ZoomableGroup disablePanning>
+      <Geographies geography={paths}>
+        {(geographies, projection) =>
+          geographies.map(geo => (
+            <Geography key={geo.id} geography={geo} projection={projection} />
+          ))
+        }
+      </Geographies>
+      <Markers>
+        {locations.map((marker, i) => (
+          <Marker
+            key={i}
+            marker={marker}
+            children={<circle cx={0} cy={0} r={5} />}
+          />
+        ))}
+      </Markers>
+    </ZoomableGroup>
+  </ComposableMap>
+)
+
+export default Map

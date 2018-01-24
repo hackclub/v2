5aeab5cc86496c16c2e0e512bad6eaf45ea7f4f5
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-24 16:28:16 -0500

    Add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Hack Club'
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-netlify'
+  ]
 }

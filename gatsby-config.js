5848c18c72d6b85e8d904fe7254313122ce11c4b
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-24 17:46:54 -0500

    Add Segment analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-segment-js',
+      options: {
+        prodKey: '35oTlU4UqlhIN8VGYmBxAzyDdfzhcscw'
+      }
+    },
     'gatsby-plugin-netlify'
   ]
 }

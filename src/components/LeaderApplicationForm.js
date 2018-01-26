6b130dd4861e4fccbea554d4479a7e2624daf0e4
Zach Latta <zach@zachlatta.com>
2018-01-25 22:13:16 -0800

    Remove redundant sentence from full address
    I think it's implied we'll be mailing stuff since we ask them to write it as if it's on an envelope.

--- src/components/LeaderApplicationForm.js
+++ src/components/LeaderApplicationForm.js
@@ -146,7 +146,7 @@ const InnerForm = props => {
           <Field
             name="leader_address"
             label="Your full address (include city, state/province, country)"
-            hint="As part of Hack Club, we'll occasionally send you physical materials (like stickers) to help market your club. Please enter your address exactly as we should write it on an envelope."
+            hint="Please enter your address exactly as we should write it on an envelope."
             onChange={handleChange}
             onBlur={handleBlur}
             value={values.leader_address}

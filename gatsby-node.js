f1543fde3518e6fd568b54e9b8bcbd775a492a56
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-28 00:21:46 -0500

    Refine Netlify redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,3 @@
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage, createRedirect } = boundActionCreators
-
-  const redirects = [
-    { f: '/find', t: '/finder' }
-  ]
-  redirects.forEach(({ f, t }) => {
-    createRedirect({
-      fromPath: f,
-      redirectInBrowser: true,
-      toPath: t || f
-    })
-  })
 }

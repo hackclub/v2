b4cbf638e922a58d1355210a4f766eafcf5b6cad
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-27 19:37:58 -0500

    Use DS colors on deprecated Icon

--- src/theme.js
+++ src/theme.js
@@ -1,37 +1,8 @@
-import palx from 'palx'
-import dotProp from 'dot-prop'
+import { theme as DS } from '@hackclub/design-system'
 
-const red = '#e42d42'
-const blue = '#2d9ce4'
-export const palette = palx(blue)
+export const colors = DS.colors
 
-export const grays = {
-  black: palette.black,
-  slate: palette.gray[8],
-  silver: palette.gray[7],
-  smoke: palette.gray[2],
-  snow: palette.gray[0],
-  white: '#ffffff'
-}
-
-export const brand = {
-  primary: red,
-  accent: palette.indigo[4],
-  success: palette.teal[5],
-  info: palette.blue[5],
-  danger: palette.red[5],
-  error: palette.red[7],
-  muted: grays.silver
-}
-
-export const colors = {
-  ...brand,
-  ...grays,
-  ...palette
-}
-
-export const cx = key =>
-  dotProp.has(colors, key) ? dotProp.get(colors, key) : key
+export const cx = DS.cx
 export const gradient = (n, from, to) =>
   `linear-gradient(${n}deg, ${cx(from)}, ${cx(to)})`
 export const geo = color => `background: ${color} url('/pattern.svg') repeat`
@@ -46,8 +17,7 @@ const theme = {
   radius: 4,
   weights: [400, 700],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72, 96],
-  font:
-    'Averta, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+  font: DS.font,
   monospace: 'SFMono-Regular, "Roboto Mono", Menlo, monospace'
 }
 

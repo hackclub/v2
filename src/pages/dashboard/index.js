07d4700aafe8c8c9b1fa923ab73f7d6d50bc27e7
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2018-01-29 22:17:50 -0500

    Improve dashboard code

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,46 +1,36 @@
-import React, { Component } from 'react'
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Text,
-  cx
-} from '@hackclub/design-system'
-import {
-  AutoSaver,
-  Field,
-  Form
-} from 'components/Forms'
+import React, { Component, Fragment } from 'react'
+import { Box, Button, Flex, Heading, Text } from '@hackclub/design-system'
+import { AutoSaver, Field, Form } from 'components/Forms'
 import Login from 'components/Login'
 import LogoutButton from 'components/LogoutButton'
 import LoadingAnimation from 'components/LoadingAnimation'
 import { Formik } from 'formik'
 import api from 'api'
-import styled from 'styled-components'
 
-const Tr = styled.tr`
+const Tr = Box.withComponent('tr').extend`
   &:nth-child(even) {
-    background: ${props => cx('snow')};
+    background: ${props => props.theme.colors.snow};
   }
 `
 
-const Td = props => {
-  const { content, children, title } = props
-
-  return (
-    <td title={title || content} style={{maxWidth: '15rem'}}>
-      <Text color={content ? 'black' : 'silver'} style={{overflow: 'auto'}}>{content || children || 'Unset'}</Text>
-    </td>
-  )
-}
+const Td = ({ content, children, title }) => (
+  <td title={title || content} style={{ maxWidth: '15rem' }}>
+    <Text color={content ? 'black' : 'silver'} style={{ overflow: 'auto' }}>
+      {content || children || 'Unset'}
+    </Text>
+  </td>
+)
 
 const Inspector = props => {
   const { authToken, application, updateApplicationList } = props
   const transformedApplication = {
     ...application,
-    interview_duration: application.interview_duration ? application.interview_duration / 60 : null,
-    interviewed_at: application.interviewed_at ? application.interviewed_at.substr(0, 10) : null
+    interview_duration: application.interview_duration
+      ? application.interview_duration / 60
+      : null,
+    interviewed_at: application.interviewed_at
+      ? application.interviewed_at.substr(0, 10)
+      : null
   }
 
   return (
@@ -48,13 +38,14 @@ const Inspector = props => {
       initialValues={transformedApplication}
       enableReinitialize={true}
       onSubmit={(values, { props, setSubmitting }) => {
-        let transformedValues = {...values}
+        const transformedValues = { ...values }
         if (values.interview_duration) {
           transformedValues.interview_duration = values.interview_duration * 60
         }
         api
           .patch(`v1/new_club_applications/${values.id}`, {
-            authToken, data: transformedValues
+            authToken,
+            data: transformedValues
           })
           .then(json => {
             setSubmitting(false)
@@ -66,48 +57,48 @@ const Inspector = props => {
       }}
     >
       {props => {
-         const {
-           handleChange,
-           handleBlur,
-           handleSubmit,
-           isSubmitting,
-           values
-         } = props
+        const {
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          values
+        } = props
 
-         return (
-           <Form onSubmit={handleSubmit}>
-             <Heading>App #{values.id}</Heading>
-             <Field
-               name="interview_notes"
-               label="Interview notes"
-               onBlur={handleBlur}
-               onChange={handleChange}
-               value={values.interview_notes}
-               type="textarea"
-             />
-             <Field
-               name="interviewed_at"
-               label="Interview date"
-               onBlur={handleBlur}
-               onChange={handleChange}
-               value={values.interviewed_at}
-               type="date"
-             />
-             <Field
-               name="interview_duration"
-               label="Interview duration (minutes)"
-               onBlur={handleBlur}
-               onChange={handleChange}
-               value={values.interview_duration}
-               type="number"
-             />
-             <AutoSaver
-               handleSubmit={handleSubmit}
-               isSubmitting={isSubmitting}
-               values={values}
-             />
-           </Form>
-         )
+        return (
+          <Form onSubmit={handleSubmit}>
+            <Heading>App #{values.id}</Heading>
+            <Field
+              name="interview_notes"
+              label="Interview notes"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.interview_notes}
+              type="textarea"
+            />
+            <Field
+              name="interviewed_at"
+              label="Interview date"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.interviewed_at}
+              type="date"
+            />
+            <Field
+              name="interview_duration"
+              label="Interview duration (minutes)"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.interview_duration}
+              type="number"
+            />
+            <AutoSaver
+              handleSubmit={handleSubmit}
+              isSubmitting={isSubmitting}
+              values={values}
+            />
+          </Form>
+        )
       }}
     </Formik>
   )
@@ -148,16 +139,17 @@ export default class extends Component {
   }
 
   updateApplicationList(updatedApplication) {
-    let { clubApplications } = this.state
+    const { clubApplications } = this.state
     clubApplications[updatedApplication.id] = updatedApplication
 
-    this.setState({clubApplications})
+    this.setState({ clubApplications })
   }
 
   pointOfContact(application) {
-    const profile = application.leader_profiles.find(profile => (
-      profile.user && profile.user.id === application.point_of_contact_id
-    ))
+    const profile = application.leader_profiles.find(
+      profile =>
+        profile.user && profile.user.id === application.point_of_contact_id
+    )
 
     return profile && profile.user.email
   }
@@ -171,7 +163,7 @@ export default class extends Component {
         return <Login userType="admin" />
       case 'success':
         return (
-          <React.Fragment>
+          <Fragment>
             <Flex justify="flex-end">
               <LogoutButton m={2} inverted={false} />
             </Flex>
@@ -188,42 +180,50 @@ export default class extends Component {
                     </tr>
                   </thead>
                   <tbody>
-                    {Object.values(clubApplications).map((application, index) => (
-                      <Tr key={index}>
-                        <Td content={application.id} />
-                        <Td content={application.high_school_name} />
-                        <Td content={application.high_school_url} />
-                        <Td content={this.pointOfContact(application)} />
-                        <Td>
-                          <Button
-                            bg="info"
-                            inverted={!selection || selection.id !== application.id}
-                            disabled={!application.submitted_at}
-                            onClick={() => {
-                              this.setState({ selection: application })
-                            }}
-                            children="✍"
-                          />
-                        </Td>
-                      </Tr>
-                    ))}
+                    {Object.values(clubApplications).map(
+                      (application, index) => (
+                        <Tr key={index}>
+                          <Td content={application.id} />
+                          <Td content={application.high_school_name} />
+                          <Td content={application.high_school_url} />
+                          <Td content={this.pointOfContact(application)} />
+                          <Td>
+                            <Button
+                              bg="info"
+                              inverted={
+                                !selection || selection.id !== application.id
+                              }
+                              disabled={!application.submitted_at}
+                              onClick={() => {
+                                this.setState({ selection: application })
+                              }}
+                              children="✍"
+                            />
+                          </Td>
+                        </Tr>
+                      )
+                    )}
                   </tbody>
                 </table>
               </Box>
-              {
-                selection &&
+              {selection && (
                 <Box>
-                  <Inspector authToken={authToken}
-                             application={selection}
-                             updateApplicationList={this.updateApplicationList}
+                  <Inspector
+                    authToken={authToken}
+                    application={selection}
+                    updateApplicationList={this.updateApplicationList}
                   />
                 </Box>
-              }
+              )}
             </Flex>
-          </React.Fragment>
+          </Fragment>
         )
       default:
-        return <Text>Something terrible has happened</Text>
+        return (
+          <Text color="error" py={3} align="center">
+            🚨 Something terrible has happened 🚨
+          </Text>
+        )
     }
   }
 }

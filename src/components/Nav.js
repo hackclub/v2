62d077379f6a2a96813ebd347507025ed71f1521
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-01 00:30:57 -0500

    Return Workshops to mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, Flex } from '@hackclub/design-system'
+import { Box, Flex, Link as A } from '@hackclub/design-system'
 import Link from 'gatsby-link'
 import { colors, mediaQueries } from '@hackclub/design-system'
 import Flag from 'components/Flag'
 
 const Base = Flex.extend.attrs({
   pt: 0,
-  px: [3, 4],
+  px: [2, 3, 4],
   pb: 2,
   justify: 'space-between',
   align: 'center',
@@ -30,19 +30,9 @@ const NavBar = Flex.withComponent('nav').extend(
       : { color: colors[props.color] || props.color }
 )
 
-export const Item = Box.withComponent('a').extend.attrs({ mx: [2, 3] })`
-  color: inherit;
-  text-decoration: none;
-  font-weight: bold;
-  text-align: center;
-
-  &:last-of-type {
-    display: none;
-    ${mediaQueries[1]} {
-      display: inline-block;
-    }
-  }
-`
+export const Item = A.extend.attrs({
+	align: 'center', bold: true, my: 1, mx: [2, 3]
+	})`color: inherit;`
 
 Item.link = Item.withComponent(Link)
 

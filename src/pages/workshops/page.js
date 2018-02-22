59ef853fa62d9905e00cd413b094174fc73b2361
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-02-21 21:10:36 -0500

    Clean up aux workshop header code

--- src/pages/workshops/page.js
+++ src/pages/workshops/page.js
@@ -34,15 +34,12 @@ const Desc = Heading.h2.extend`
   text-shadow: 0 1px 2px rgba(0, 0, 0, 0.32);
 `
 
-const EditLink = A.extend.attrs({
-  color: "white",
-  f: 3,
-})`
+const EditLink = A.extend.attrs({ color: 'white', f: 3 })`
   display: none;
 
   position: absolute;
-  bottom: 20px;
-  right: 30px;
+  bottom: ${props => props.theme.space[3]}px;
+  right: ${props => props.theme.space[4]}px;
 
   ${mediaQueries.md} {
     display: inline-block;
@@ -53,7 +50,7 @@ const Body = Container.withComponent(MarkdownBody)
 A.link = A.withComponent(Link)
 Section.h = Section.withComponent('header')
 
-function generateSubtitle(description, authorText) {
+const generateSubtitle = (description, authorText) => {
   if (!authorText) {
     return description
   }
@@ -61,30 +58,25 @@ function generateSubtitle(description, authorText) {
   // This iterates over each word in authorText, finds GitHub usernames (any
   // text that looks like "@orpheus", and turns them into links.
   const parsedAuthorText = authorText.split(' ').map((word, index, arr) => {
-    let processedWord;
-
     const matches = word.match(/@(\w+)/)
 
-    if (matches) {
-      const username = matches[1]
-
-      processedWord = (
-        <A href={`https://github.com/${username}`}
-           target="_blank"
-           color="white">
-           {word}
-        </A>
-      )
-    } else {
-      processedWord = word
-    }
+    const processedWord = matches ? (
+      <A
+        href={`https://github.com/${matches[1]}`}
+        target="_blank"
+        color="white"
+        children={word}
+      />
+    ) : (
+      word
+    )
 
     // This pads returned results with spaces, making our final array look the
     // following:
     //
     //   [ 'Hack', ' ', 'Club', ' ', 'staff' ]
     //
-    if (index === arr.length - 1) { 
+    if (index === arr.length - 1) {
       // if last item in array, don't give an extra space
       return processedWord
     } else {
@@ -93,15 +85,14 @@ function generateSubtitle(description, authorText) {
   })
 
   return (
-    <React.Fragment>
+    <Fragment>
       {description}. Created by {parsedAuthorText}.
-    </React.Fragment>
+    </Fragment>
   )
 }
 
-function githubEditUrl(slug) {
-  return `https://github.com/hackclub/hackclub/edit/master${slug}/README.md`
-}
+const githubEditUrl = slug =>
+  `https://github.com/hackclub/hackclub/edit/master${slug}/README.md`
 
 export default ({ data: { markdownRemark } }) => {
   if (markdownRemark) {

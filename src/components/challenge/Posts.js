fd363a0b0c502df758756dc593d39c9189023f78
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-03-26 14:59:22 -0400

    Reduce upvoting code

--- src/components/challenge/Posts.js
+++ src/components/challenge/Posts.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react'
 import axios from 'axios'
 import Post from 'components/challenge/Post'
 import storage from 'storage'
-import { map, includes, isEmpty, filter } from 'lodash'
+import { map, includes, isEmpty, remove, filter } from 'lodash'
 
 class Posts extends Component {
   constructor() {
@@ -17,7 +17,7 @@ class Posts extends Component {
     this.setState({ userId, userEmail })
     this.api = axios.create({
       baseURL: 'https://api.hackclub.com/',
-      headers: { 'Authorization': `Bearer ${authToken}` }
+      headers: { Authorization: `Bearer ${authToken}` }
     })
 
     this.refreshPosts()
@@ -30,8 +30,9 @@ class Posts extends Component {
       let posts = res.data || []
 
       // array of post ids that user has upvoted
-      let upvotes = []
+      const upvotes = []
       posts.forEach(post => {
+        post.upvotesCount = post.upvotes.length
         post.upvotes.forEach(upvote => {
           if (upvote.user.id == userId) {
             upvotes.push(post.id)
@@ -40,10 +41,6 @@ class Posts extends Component {
       })
 
       console.log(upvotes)
-      posts.map(post => {
-        post.upvotesCount = post.upvotes.length
-        return post
-      })
 
       // sort by upvote count
       posts = posts.sort((p1, p2) => {
@@ -58,56 +55,45 @@ class Posts extends Component {
   onUpvote(e, postId) {
     const { userId: authUser } = this.state
     if (isEmpty(authUser)) return
+    let { upvotes, posts } = this.state
+    let post = posts.find(post => post.id == postId)
+    let postIndex = posts.indexOf(post)
+    console.log(this.api)
     if (includes(this.state.upvotes, postId)) {
       // if this is nil, this means we've ran into a race where this.state.posts
       // hasn't finished updating (probably from a this.refreshPosts call) -
       // just ignore the user action for the time being and let them retry once
       // it actually updates
       const upvote = this.state.posts
-        .find(post => post.id == postId).upvotes
-        .find(upvote => upvote.user.id == authUser)
+        .find(post => post.id == postId)
+        .upvotes.find(upvote => upvote.user.id == authUser)
 
-      if (!upvote) {
-        return
-      }
+      if (!upvote) return
 
       // immediately increment values so it feels responsive, later refresh
       // posts completely so we have the correct values in state
+      // remove post from upvotes array w/ removed postId
+      let truncatedUpvotes = remove(upvotes, postId)
+      this.setState({ upvotes: truncatedUpvotes })
 
-      // remove post from upvotes array //
-      let upvotes = this.state.upvotes
-      // w/ removed postId
-      let truncatedUpvotes = upvotes.splice(upvotes.indexOf(postId), 1)
-      this.setState(upvotes: truncatedUpvotes)
-
-      // increment upvotesCount //
-      let posts = this.state.posts
-      let post = posts.find(post => post.id == postId)
-      let postIndex = posts.indexOf(post)
+      // decrement upvotesCount //
       post.upvotesCount--
       posts[postIndex] = post
-      this.setState(posts: posts)
+      this.setState({ posts })
 
       // actually make requests & refresh
       this.api.delete(`v1/upvotes/${upvote.id}`).then(res => {
         this.refreshPosts()
       })
     } else {
-      // same deal here, immediately update state so it displays correctly then
-      // do a complete refresh to ensure we have good values
- 
       // add post to upvotes array //
-      let upvotes = this.state.upvotes
       upvotes.push(postId)
-      this.setState(upvotes: upvotes)
+      this.setState({ upvotes })
 
       // increment upvotesCount //
-      let posts = this.state.posts
-      let post = posts.find(post => post.id == postId)
-      let postIndex = posts.indexOf(post)
       post.upvotesCount++
       posts[postIndex] = post
-      this.setState(posts: posts)
+      this.setState({ posts })
 
       // actually make requests & refresh
       this.api.post(`v1/posts/${postId}/upvotes`).then(res => {

3b245badc16fe4a5eedce0184c8571bd1b520af5
Max Wofford <max@maxwofford.com>
2018-04-18 10:53:03 -0500

    Include applicant emails on application dashboard

--- src/components/admin/Information.js
+++ src/components/admin/Information.js
@@ -2,5 +2,12 @@ import React from 'react'
 import ClubApplicationForm from 'components/apply/ClubApplicationForm'
 
 export default props => (
-  <ClubApplicationForm params={props.application} disableAutosave />
+  <React.Fragment>
+    <ul>
+      {props.application.leader_profiles.map((app, index) => (
+        <li key={index}>{app.user.email}</li>
+      ))}
+    </ul>
+    <ClubApplicationForm params={props.application} disableAutosave />
+  </React.Fragment>
 )

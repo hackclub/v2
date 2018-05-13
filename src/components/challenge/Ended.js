6bb7cbd8d13997495658c9f490954eb605882324
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-05-13 18:02:32 -0400

    Change Ended icon

--- src/components/challenge/Ended.js
+++ src/components/challenge/Ended.js
@@ -8,7 +8,7 @@ const Sheet = Container.withComponent(Card).extend`
 
 const Ended = () => (
   <Sheet maxWidth={32} bg="yellow.2" p={[2, 3, 4]} mt={-3} mb={4}>
-    <Icon name="archive" size={48} mr={[2, 3]} color="orange.7" />
+    <Icon name="mood_bad" size={48} mr={[2, 3]} color="orange.7" />
     <Box color="orange.8">
       <Text f={3} bold>
         This Challenge has ended.

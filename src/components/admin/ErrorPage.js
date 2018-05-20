9b2b888920b993332c5e2fe88cafdcbbbb0b6610
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-05-20 14:51:13 -0400

    Name ErrorPage export

--- src/components/admin/ErrorPage.js
+++ src/components/admin/ErrorPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Text } from '@hackclub/design-system'
 
-export default () => (
-  <Text color="error" py={3} align="center">
+const ErrorPage = props => (
+  <Text color="error" py={3} align="center" {...props}>
     🚨 Something terrible has happened 🚨
     <br />
     Please let us know about this by emailing us at{' '}
     <a href="mailto:itsbroken@hackclub.com">itsbroken@hackclub.com</a>
   </Text>
 )
+export default ErrorPage

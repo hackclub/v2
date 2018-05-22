b3829ff55da49abceacb8ab7e59c43710465bb41
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2018-05-21 23:03:38 -0400

    Add closed prop to Form

--- src/components/challenge/Form.js
+++ src/components/challenge/Form.js
@@ -5,19 +5,13 @@ import PostForm from 'components/challenge/PostForm'
 import LoadingAnimation from 'components/LoadingAnimation'
 import ErrorMessage from 'components/admin/ErrorPage'
 
-export default ({ status, challengeId }) => {
+const Form = ({ status, closed = true, challengeId }) => {
   switch (status) {
     case 'loading':
       return <LoadingAnimation />
     case 'success':
-      return (
+      return closed ? (
         <Fragment>
-          {/*
-          <Heading.h2 mt={0} mb={2} f={[3, 4]}>
-            Post your project
-          </Heading.h2>
-          <PostForm challengeId={challengeId} />
-          */}
           <Heading.h2 mt={0} mb={2} f={4}>
             Submissions closed!
           </Heading.h2>
@@ -26,12 +20,19 @@ export default ({ status, challengeId }) => {
             (5/23).
           </Text>
         </Fragment>
+      ) : (
+        <Fragment>
+          <Heading.h2 mt={0} mb={2} f={[3, 4]}>
+            Post your project
+          </Heading.h2>
+          <PostForm challengeId={challengeId} />
+        </Fragment>
       )
     case 'needsToAuth':
       return (
         <Fragment>
           <Heading.h2 mt={0} mb={3} f={[3, 4]}>
-            Sign in to post + upvote
+            Sign in to {!closed && 'post + '}upvote
           </Heading.h2>
           <LoginForm
             bg="black"
@@ -45,3 +46,5 @@ export default ({ status, challengeId }) => {
       return <ErrorMessage />
   }
 }
+
+export default Form

0f8db6199346ef03d0562c42263718799192ef83
Max Wofford <max@maxwofford.com>
2018-05-24 00:44:39 -0700

    Switch back to production API endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import fetch from 'unfetch'
 import storage from 'storage'
 
-const apiBase = 'https://221c216b.ngrok.io/'
+const apiBase = 'https://api.hackclub.com/'
 const methods = ['GET', 'PUT', 'POST', 'PATCH', 'DELETE']
 
 const generateMethod = method => (path, options = {}, fetchOptions = {}) => {

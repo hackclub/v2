87c54886bc138b19dce8f669d8432eaa4e7eea06
Max Wofford <max@maxwofford.com>
2018-05-28 18:41:28 -0700

    Indicate leader checkup state by changing submit button

--- src/components/checkup/LeaderForm.js
+++ src/components/checkup/LeaderForm.js
@@ -10,19 +10,22 @@ const submitStatus = status =>
     success: 'Success',
     redirect: 'Success',
     loading: 'Loading...',
-    clean: 'Verify'
+    clean: 'Verify',
+    invalid: 'Invalid'
   }[status] || 'Update')
 const submitColor = status =>
   ({
     success: 'success',
     redirect: 'success',
     loading: 'warning',
-    clean: 'info'
+    clean: 'info',
+    invalid: 'error'
   }[status] || 'primary')
 
 const InnerForm = ({
   dirty,
   redirectUrl,
+  isValid,
   status,
   values,
   errors,
@@ -31,187 +34,200 @@ const InnerForm = ({
   handleBlur,
   handleSubmit,
   isSubmitting
-}) => (
-  <form onSubmit={handleSubmit}>
-    <Field
-      name="new_leader_name"
-      value={values.new_leader_name}
-      label="Full Name"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_name && errors.new_leader_name}
-      mb={2}
-    />
-    <Field
-      name="new_leader_phone_number"
-      value={values.new_leader_phone_number}
-      label="Phone Number (including country code if outside the US)"
-      placeholder="(415)-555-555"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_phone_number && errors.new_leader_phone_number}
-      mb={2}
-    />
-    <Field
-      name="email"
-      value={values.email}
-      label="Email Address"
-      placeholder="email"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.email && errors.email}
-      mb={2}
-    />
-    <Field
-      name="username"
-      value={values.username}
-      label="Hack Club Username (public)"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.username && errors.username}
-      mb={2}
-    />
-    <Field
-      name="new_leader_address"
-      value={values.new_leader_address}
-      label="Full Address"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_address && errors.new_leader_address}
-      mb={2}
-    />
-    <Field
-      name="new_leader_gender"
-      value={values.new_leader_gender || 'select'}
-      label="Gender"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_gender && errors.new_leader_gender}
-      mb={2}
-      type="select"
-    >
-      <option value="select" disabled>
-        Select One
-      </option>
-      <option value="male">Male</option>
-      <option value="female">Female</option>
-      <option value="genderqueer">Genderqueer</option>
-      <option value="agender">Agender</option>
-      <option value="other_gender">Other gender</option>
-    </Field>
-    <Field
-      name="new_leader_birthday"
-      value={values.new_leader_birthday}
-      label="Birthday"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_birthday && errors.new_leader_birthday}
-      mb={2}
-    />
-    <Field
-      name="new_leader_ethnicity"
-      value={values.new_leader_ethnicity || 'select'}
-      label="Ethnicity"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_ethnicity && errors.new_leader_ethnicity}
-      mb={2}
-      type="select"
-    >
-      <option value="select" disabled>
-        Select One
-      </option>
-      <option value="hispanic_or_latino">Hispanic or Latino</option>
-      <option value="white">White</option>
-      <option value="black">Black</option>
-      <option value="native_american_or_indian">
-        Native American or Indian
-      </option>
-      <option value="asian_or_pacific_islander">
-        Asian or Pacific Islander
-      </option>
-      <option value="other_ethnicity">Other ethnicity</option>
-    </Field>
-    <Field
-      name="new_leader_expected_graduation"
-      value={values.new_leader_expected_graduation}
-      label="Expected Graduation Date"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={
-        touched.new_leader_expected_graduation &&
-        errors.new_leader_expected_graduation
-      }
-      mb={2}
-    />
-    <Field
-      name="new_leader_personal_website"
-      value={values.new_leader_personal_website}
-      label="Personal Website"
-      placeholder="https://"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={
-        touched.new_leader_personal_website &&
-        errors.new_leader_personal_website
-      }
-      mb={2}
-    />
-    <Field
-      name="new_leader_github_url"
-      value={values.new_leader_github_url}
-      label="GitHub profile"
-      placeholder="https://"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_github_url && errors.new_leader_github_url}
-      mb={2}
-    />
-    <Field
-      name="new_leader_linkedin_url"
-      value={values.new_leader_linkedin_url}
-      label="LinkedIn Profile"
-      placeholder="https://"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_linkedin_url && errors.new_leader_linkedin_url}
-      mb={2}
-    />
-    <Field
-      name="new_leader_twitter_url"
-      value={values.new_leader_twitter_url}
-      label="Twitter Profile"
-      placeholder="https://"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_twitter_url && errors.new_leader_twitter_url}
-      mb={2}
-    />
-    <Field
-      name="new_leader_facebook_url"
-      value={values.new_leader_facebook_url}
-      label="Facebook Profile"
-      placeholder="https://"
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.new_leader_facebook_url && errors.new_leader_facebook_url}
-      mb={2}
-    />
-    {/* TODO: Update submit button */}
-    <Submit
-      disabled={isSubmitting}
-      onClick={handleSubmit}
-      value={submitStatus(!dirty ? 'clean' : isSubmitting ? 'loading' : status)}
-      bg={submitColor(!dirty ? 'clean' : isSubmitting ? 'loading' : status)}
-      f={4}
-    />
-    {status === 'redirecting' && (
-      <Text>
-        Redirecting you to <Link href={redirectUrl}>the next step</Link>.
-      </Text>
-    )}
-  </form>
-)
+}) => {
+  let buttonState = ''
+  if (dirty) {
+    buttonState = isSubmitting ? 'loading' : status
+  } else {
+    var interacted = Object.keys(touched).length > 0
+    buttonState = isValid || !interacted ? 'clean' : 'invalid'
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field
+        name="new_leader_name"
+        value={values.new_leader_name}
+        label="Full Name"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.new_leader_name && errors.new_leader_name}
+        mb={2}
+      />
+      <Field
+        name="new_leader_phone_number"
+        value={values.new_leader_phone_number}
+        label="Phone Number (including country code if outside the US)"
+        placeholder="(415)-555-555"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={
+          touched.new_leader_phone_number && errors.new_leader_phone_number
+        }
+        mb={2}
+      />
+      <Field
+        name="email"
+        value={values.email}
+        label="Email Address"
+        placeholder="email"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.email && errors.email}
+        mb={2}
+      />
+      <Field
+        name="username"
+        value={values.username}
+        label="Hack Club Username (public)"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.username && errors.username}
+        mb={2}
+      />
+      <Field
+        name="new_leader_address"
+        value={values.new_leader_address}
+        label="Full Address"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.new_leader_address && errors.new_leader_address}
+        mb={2}
+      />
+      <Field
+        name="new_leader_gender"
+        value={values.new_leader_gender || 'select'}
+        label="Gender"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.new_leader_gender && errors.new_leader_gender}
+        mb={2}
+        type="select"
+      >
+        <option value="select">Select One</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="genderqueer">Genderqueer</option>
+        <option value="agender">Agender</option>
+        <option value="other_gender">Other gender</option>
+      </Field>
+      <Field
+        name="new_leader_birthday"
+        value={values.new_leader_birthday}
+        label="Birthday"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.new_leader_birthday && errors.new_leader_birthday}
+        mb={2}
+      />
+      <Field
+        name="new_leader_ethnicity"
+        value={values.new_leader_ethnicity || 'select'}
+        label="Ethnicity"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.new_leader_ethnicity && errors.new_leader_ethnicity}
+        mb={2}
+        type="select"
+      >
+        <option value="select">Select One</option>
+        <option value="hispanic_or_latino">Hispanic or Latino</option>
+        <option value="white">White</option>
+        <option value="black">Black</option>
+        <option value="native_american_or_indian">
+          Native American or Indian
+        </option>
+        <option value="asian_or_pacific_islander">
+          Asian or Pacific Islander
+        </option>
+        <option value="other_ethnicity">Other ethnicity</option>
+      </Field>
+      <Field
+        name="new_leader_expected_graduation"
+        value={values.new_leader_expected_graduation}
+        label="Expected Graduation Date"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={
+          touched.new_leader_expected_graduation &&
+          errors.new_leader_expected_graduation
+        }
+        mb={2}
+      />
+      <Field
+        name="new_leader_personal_website"
+        value={values.new_leader_personal_website}
+        label="Personal Website"
+        placeholder="https://"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={
+          touched.new_leader_personal_website &&
+          errors.new_leader_personal_website
+        }
+        mb={2}
+      />
+      <Field
+        name="new_leader_github_url"
+        value={values.new_leader_github_url}
+        label="GitHub profile"
+        placeholder="https://"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.new_leader_github_url && errors.new_leader_github_url}
+        mb={2}
+      />
+      <Field
+        name="new_leader_linkedin_url"
+        value={values.new_leader_linkedin_url}
+        label="LinkedIn Profile"
+        placeholder="https://"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={
+          touched.new_leader_linkedin_url && errors.new_leader_linkedin_url
+        }
+        mb={2}
+      />
+      <Field
+        name="new_leader_twitter_url"
+        value={values.new_leader_twitter_url}
+        label="Twitter Profile"
+        placeholder="https://"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.new_leader_twitter_url && errors.new_leader_twitter_url}
+        mb={2}
+      />
+      <Field
+        name="new_leader_facebook_url"
+        value={values.new_leader_facebook_url}
+        label="Facebook Profile"
+        placeholder="https://"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={
+          touched.new_leader_facebook_url && errors.new_leader_facebook_url
+        }
+        mb={2}
+      />
+      {/* TODO: Update submit button */}
+      <Submit
+        disabled={isSubmitting}
+        onClick={handleSubmit}
+        value={submitStatus(buttonState)}
+        bg={submitColor(buttonState)}
+        my={3}
+        w={1}
+        f={4}
+      />
+      {status === 'redirecting' && (
+        <Text>
+          Redirecting you to <Link href={redirectUrl}>the next step</Link>.
+        </Text>
+      )}
+    </form>
+  )
+}
 
 export default withFormik({
   mapPropsToValues: props => ({
@@ -222,9 +238,9 @@ export default withFormik({
     email_on_new_challenges: props.email_on_new_challenges,
     new_leader_username: props.new_leader.username || '',
     new_leader_address: props.new_leader.address || '',
-    new_leader_gender: props.new_leader.gender || '',
+    new_leader_gender: props.new_leader.gender || 'select',
     new_leader_birthday: props.new_leader.birthday || '',
-    new_leader_ethnicity: props.new_leader.ethnicity || '',
+    new_leader_ethnicity: props.new_leader.ethnicity || 'select',
     new_leader_expected_graduation: props.new_leader.expected_graduation || '',
     new_leader_facebook_url: props.new_leader.facebook_url || '',
     new_leader_github_url: props.new_leader.github_url || '',
@@ -246,14 +262,13 @@ export default withFormik({
     new_leader_address: yup.string().required('required'),
     new_leader_gender: yup
       .string()
-      .required('required')
-      .matches(/(male|female|genderqueer|agender|other_gender)/),
+      .matches(/(male|female|genderqueer|agender|other_gender)/, 'required'),
     new_leader_birthday: yup.date().nullable(),
     new_leader_ethnicity: yup
       .string()
-      .required('required')
       .matches(
-        /(hispanic_or_latino|white|black|native_american_or_indian|asian_or_pacific_islander|other_ethnicity)/
+        /(hispanic_or_latino|white|black|native_american_or_indian|asian_or_pacific_islander|other_ethnicity)/,
+        'required'
       ),
     new_leader_expected_graduation: yup.date().required('required'),
     new_leader_facebook_url: yup

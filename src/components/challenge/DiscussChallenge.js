69f7ad984f37aaff51cb4e45a8310ee6dfa24d30
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-08-09 23:52:06 -0400

    Use Sheet on Ended + DiscussChallenge

--- src/components/challenge/DiscussChallenge.js
+++ src/components/challenge/DiscussChallenge.js
@@ -1,23 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Container, Card, Icon, Box, Flex, Text } from '@hackclub/design-system'
+import Sheet from 'components/Sheet'
 import DiscussOnSlack from 'components/DiscussOnSlack'
 
-const Sheet = Container.withComponent(Card).extend`
+const Root = styled(Sheet)`
   display: flex;
   align-items: center;
-  flex-wrap: wrap;
 `
 
 const DiscussChallenge = () => (
-  <Sheet maxWidth={36} mt={[3, 4]} py={3} px={[3, 4]} bg="red.0">
+  <Root maxWidth={36} mt={[3, 4]} p={[2, 3]} bg="red.0">
     <Flex align="center" flex="1 1 auto" mb={[3, 0]}>
-      <Icon name="forum" size={24} mr={[2, 3]} color="primary" />
-      <Text color="primary" f={2}>
-        Join the conversation in <Text.span bold>#challenges</Text.span>
-      </Text>
+      <Icon name="forum" size={32} mr={[2, 3]} color="primary" />
+      <Box align="left" color="primary">
+        <Text f={2}>Join the conversation</Text>
+        <Text f={3} bold>
+          #challenges
+        </Text>
+      </Box>
     </Flex>
     <DiscussOnSlack f={2} />
-  </Sheet>
+  </Root>
 )
 
 export default DiscussChallenge
--- src/components/challenge/Ended.js
+++ src/components/challenge/Ended.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
 import { Container, Card, Icon, Box, Text } from '@hackclub/design-system'
+import Sheet from 'components/Sheet'
 
-const Sheet = Container.withComponent(Card).extend`
+const Root = styled(Sheet)`
   display: flex;
   align-items: center;
 `
 
 const Ended = () => (
-  <Sheet maxWidth={36} bg="yellow.1" p={[2, 3]} mt={-3} mb={3}>
     <Icon name="hourglass_empty" size={48} mr={[2, 3]} color="warning" />
-    <Box color="orange.6">
+  <Root maxWidth={36} bg="teal.1" p={[2, 3]} mb={4}>
+    <Box color="teal.9">
       <Text f={3} bold>
         This challenge has ended.
       </Text>
       <Text f={2}>Check back next week for the new challenge!</Text>
     </Box>
-  </Sheet>
+  </Root>
 )
 
 export default Ended

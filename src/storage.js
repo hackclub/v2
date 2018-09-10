86379168fa592d69264d1acc8a93867532599dd0
Max Wofford <max@maxwofford.com>
2018-09-10 12:10:18 -0700

    Attempt to fix storage parsing errors

--- src/storage.js
+++ src/storage.js
@@ -1,5 +1,16 @@
 export default {
-  get: key => JSON.parse(localStorage.getItem(key)),
+  get: key => {
+    try {
+      // (max@maxwofford.com) Values that were set before values were stringified might fail to parse, so we return the raw storage item if we can't parse it
+      return JSON.parse(localStorage.getItem(key))
+    } catch (e) {
+      if (e.name === 'SyntaxError') {
+        return localStorage.getItem(key)
+      } else {
+        console.error(e)
+      }
+    }
+  },
   set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
   remove: key => localStorage.removeItem(key)
 }

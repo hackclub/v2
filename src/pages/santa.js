92d3f86a4c1b6fa95edc143096fdc7437f0740ef
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2018-12-11 15:35:09 -0500

    Fix quote

--- src/pages/santa.js
+++ src/pages/santa.js
@@ -83,9 +83,9 @@ export default () => (
           Find your holiday zen with Hack&nbsp;Club Secret&nbsp;Santa.
         </Megaline>
         <Lead fontSize={[3, null, 4]} color="snow" maxWidth={48} my={3} mx={0}>
-          Sign up is now closed. Check your email for the next steps! 
-          Santa's list will be finalized by December 12. You can also join #secret-santa
-          on Slack to participate in the discussion!
+          Signup is now closed. Check your email for the next steps! 
+          Santa’s list will be finalized by December 12. You can also join #secret-santa
+          on Slack to participate in the discussion.
         </Lead>
         <IconButton
           is={LargeButton.withComponent(Link)}

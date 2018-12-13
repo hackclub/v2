14e85bf3b046dd46246e71d28a6174c65daa3e77
Max Wofford <max@maxwofford.com>
2018-12-13 17:28:56 -0500

    Rules button doesn't cause page to jump

--- src/components/challenge/Help.js
+++ src/components/challenge/Help.js
@@ -3,6 +3,19 @@ import { Heading, Text, Link } from '@hackclub/design-system'
 import IconButton from 'components/IconButton'
 import { Modal, Overlay, CloseButton } from 'components/Modal'
 
+const RulesButton = props => (
+  <IconButton
+    glyph="flag-fill"
+    bg="info"
+    children="Rules"
+    inverted
+    fontSize={2}
+    m={3}
+    style={{ position: 'absolute', right: 0, top: 0 }}
+    {...props}
+  />
+)
+
 class Help extends Component {
   state = { active: false }
 
@@ -15,29 +28,13 @@ class Help extends Component {
     if (this.state.active) {
       return (
         <Fragment>
+          <RulesButton onClick={this.toggleRules} />
           <Modal align="left" my={4} p={[3, 4]}>
             <CloseButton onClick={this.toggleRules} />
             <Heading.h2>Challenge Rules</Heading.h2>
-            {/*
-            <Text
-              fontSize={2}
-              mt={3}
-              color="info"
-              py={2}
-              px={3}
-              bg="blue.0"
-              style={{ borderRadius: 4 }}
-            >
-              For this challenge, your entry must use{' '}
-              <Link href="https://p5js.org" target="_blank" bold>
-                p5.js
-              </Link>{' '}
-              in its code.
-            </Text>
-            */}
             <Text fontSize={2} my={3}>
-              Challenge is open to Hack Club community members. It
-              strictly follows Hack Club’s{' '}
+              Challenge is open to Hack Club community members. It strictly
+              follows Hack Club’s{' '}
               <Link href="https://conduct.hackclub.com" target="_blank">
                 Code of Conduct
               </Link>
@@ -58,19 +55,7 @@ class Help extends Component {
         </Fragment>
       )
     }
-    return (
-      <IconButton
-        glyph="flag-fill"
-        bg="info"
-        children="Rules"
-        inverted
-        fontSize={2}
-        m={3}
-        onClick={this.toggleRules}
-        style={{ position: 'absolute', right: 0, top: 0 }}
-        {...this.props}
-      />
-    )
+    return <RulesButton onClick={this.toggleRules} />
   }
 }
 

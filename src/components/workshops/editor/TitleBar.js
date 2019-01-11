4d5fd513948b9121bdbed8edf0969fb2dbc505c9
merelinguist <merelinguist@users.noreply.github.com>
2019-01-11 11:24:59 +0000

    Name editing

--- src/components/workshops/editor/TitleBar.js
+++ src/components/workshops/editor/TitleBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import {
   Flex,
@@ -11,6 +11,8 @@ import {
 } from '@hackclub/design-system'
 import Link from 'gatsby-link'
 
+import MetaModal from './MetaModal'
+
 const Base = styled(Flex)`
   background-color: rgba(255, 255, 255, 0.7);
   box-shadow: ${theme.boxShadows[0]};
@@ -20,39 +22,65 @@ const Base = styled(Flex)`
     white-space: nowrap;
     text-overflow: ellipsis;
     line-height: 1;
+    cursor: pointer;
 
     ${mediaQueries.sm} {
+      transition: ${theme.transition};
       background-image: linear-gradient(
         180deg,
         transparent 65%,
         ${hexa('red.3', 0.75)} 0
       );
+
+      &:hover {
+        background-image: linear-gradient(
+          180deg,
+          transparent 50%,
+          ${hexa('red.3', 0.75)} 0
+        );
+      }
     }
   }
 
-  a > a {
+  a:nth-of-type(2) {
     background-color: ${hexa('info', 0.2)} !important;
     color: ${theme.colors.info};
   }
 `
 
-export default ({ name }) => (
-  <Base p={3} align="center" justify="space-between">
-    <Link to="/workshops/drafts">
-      <IconButton
-        glyph="view-back"
-        bg="smoke"
-        color="black"
-        circle
-        p={1}
-        style={{ opacity: 0.7 }}
-      />
-    </Link>
-    <Text fontSize={[2, 3]} bold mx={3}>
-      {name}
-    </Text>
-    <Button bg="info" scale>
-      Publish
-    </Button>
-  </Base>
-)
+export default class extends Component {
+  state = { active: false }
+
+  toggleModal = () => {
+    this.setState(state => ({ active: !state.active }))
+  }
+
+  render() {
+    const { active } = this.state
+    const { name } = this.props
+
+    return (
+      <Fragment>
+        <MetaModal active={active} toggleModal={this.toggleModal} name={name} />
+        <Base p={3} align="center" justify="space-between">
+          <Link to="/workshops/drafts">
+            <IconButton
+              glyph="view-back"
+              bg="smoke"
+              color="black"
+              circle
+              p={1}
+              style={{ opacity: 0.7 }}
+            />
+          </Link>
+          <Text onClick={this.toggleModal} fontSize={[2, 3]} bold mx={3}>
+            {name}
+          </Text>
+          <Button bg="info" scale>
+            Publish
+          </Button>
+        </Base>
+      </Fragment>
+    )
+  }
+}

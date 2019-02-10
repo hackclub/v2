28ebb377342a56019a34493c6c8390a671344f03
Lachlan Campbell <lachlan@hackclub.com>
2019-02-10 13:27:39 -0500

    Redesign workshop header

--- src/templates/workshop.js
+++ src/templates/workshop.js
@@ -6,7 +6,6 @@ import {
   Flex,
   Heading,
   Icon,
-  Image,
   Link as A,
   Section,
   Text,
@@ -22,8 +21,8 @@ import {
   Breadcrumb,
   BreadcrumbDivider
 } from 'components/Breadcrumbs'
-import IconButton from 'components/IconButton'
 import MarkdownBody from 'components/MarkdownBody'
+import Author from 'components/workshops/Author'
 import DiscussOnSlack from 'components/DiscussOnSlack'
 import ShareButton from 'components/ShareButton'
 import Sheet from 'components/Sheet'
@@ -45,22 +44,10 @@ const OnlyOnPrint = styled(Box)`
 `
 
 const Header = styled(Box.withComponent('header'))`
-  li a,
-  h2,
-  p {
-    text-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
-  }
-`
+  border-bottom: 1px solid ${theme.colors.smoke};
 
-const Name = styled(Heading.h1).attrs({ px: 4, bg: 'white' })`
-  color: black;
-  mix-blend-mode: screen;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.75);
-  clip-path: polygon(4% 0%, 100% 0%, 96% 100%, 0% 100%);
-  display: inline-block;
-  width: min-content;
-  ${theme.mediaQueries.sm} {
-    width: max-content;
+  h2 {
+    font-weight: 400;
   }
 `
 
@@ -93,6 +80,7 @@ const TocHeading = styled(Text.withComponent('summary')).attrs({
 })`
   cursor: pointer;
   line-height: 1;
+  user-select: none;
   &::-webkit-details-marker {
     display: none;
   }
@@ -124,33 +112,6 @@ const Body = styled(Container.withComponent(MarkdownBody))`
 `
 A.link = A.withComponent(Link)
 Section.h = Section.withComponent('header')
-
-const linkAuthor = authorText => {
-  if (isEmpty(authorText)) return null
-  // This iterates over each word in authorText, finds GitHub usernames (any
-  // text that looks like "@orpheus", and turns them into links.
-  const parsedAuthorText = authorText.split(' ').map((word, index, arr) => {
-    const matches = word.match(/@(\w+)/)
-    const processedWord = matches ? (
-      <A
-        href={`https://github.com/${matches[1]}`}
-        target="_blank"
-        color="inherit"
-        children={word}
-      />
-    ) : (
-      word
-    )
-
-    // This pads returned results with spaces, making our final array look the
-    // following:
-    //   [ 'Hack', ' ', 'Club', ' ', 'staff' ]
-    // if last item in array, don't give an extra space
-    return index === arr.length - 1 ? processedWord : [processedWord, ' ']
-  })
-
-  return <Fragment>Created by {parsedAuthorText}</Fragment>
-}
 
 const CardsSection = styled(Box)`
   background-image: linear-gradient(
@@ -286,21 +247,12 @@ export default ({ data }) => {
         {canonical && <link rel="canonical" href={canonical} />}
         <script type="application/ld+json" children={JSON.stringify(schema)} />
       </Helmet>
-      <Nav />
+      <Nav color="slate" />
       <NotOnPrint>
-        <Header
-          bg="accent"
-          color="white"
-          p={0}
-          align="center"
-          style={{
-            backgroundImage: `url('${bg}')`,
-            position: 'relative'
-          }}
-        >
-          <Container pt={[5, 6]} px={2}>
+        <Header color="black" p={0} align="center">
+          <Container pt={[5, 6]} pb={4} px={2}>
             <Breadcrumbs align="center" justify="center" mt={3} mb={2} wrap>
-              <Breadcrumb to="/workshops" name="All Workshops" position={1} />
+              <Breadcrumb to="/workshops" name="Workshops" position={1} />
               <BreadcrumbDivider />
               <Breadcrumb
                 to={`/workshops#${group}`}
@@ -309,74 +261,59 @@ export default ({ data }) => {
               />
               <BreadcrumbDivider />
             </Breadcrumbs>
-            <Name fontSize={6} mb={3} children={name} />
+            <Heading.h1 fontSize={6} mb={2} children={name} />
             <Heading.h2 fontSize={4} children={description} />
-            <Text
-              fontSize={2}
-              caps
-              mt={3}
-              mb={[3, 0]}
-              children={linkAuthor(author)}
-            />
+            <Text fontSize={2} color="muted" mt={3} mb={[3, 0]}>
+              <Author text={author} />
+            </Text>
           </Container>
-          <Flex width={1} px={3} pb={3} justify="center">
-            <IconButton
-              bg="slate"
-              glyph="edit"
-              children="Edit"
-              inverted
-              href={githubEditUrl(slug)}
-              target="_blank"
-              fontSize={2}
-              my={1}
-            />
-          </Flex>
         </Header>
       </NotOnPrint>
-      <OnlyOnPrint p={3}>
-        <Flex align="center" justify="flex-end" my={3}>
-          <Image
-            src="/logo-red.svg"
-            width={`${theme.space[6]}px`}
-            mr={3}
-            alt="Hack Club logo"
-          />
-          <strong>Computer Science Tutorials</strong>
-        </Flex>
-        <Heading.h1 mt={4} mb={3} children={name} />
-        <Heading.h2 fontSize={4} children={description} />
-        <Text color="muted" my={3} children={linkAuthor(author)} />
-        <Text color="muted">
-          You can find this tutorial online at <u>{url}</u>
-        </Text>
+      <OnlyOnPrint>
+        <Header color="black" p={3} pb={4}>
+          <Flex align="center" mb={3}>
+            <Text caps bold>
+              <Text.span color="primary">Hack Club</Text.span> – Computer
+              Science Tutorials
+            </Text>
+          </Flex>
+          <Heading.h1 mt={4} mb={2} children={name} />
+          <Heading.h2 fontSize={4} mb={3} children={description} />
+          <Author text={author} justify="flex-start" />
+          <Text color="muted" mt={1}>
+            Find this tutorial online at <u>{url}</u>
+          </Text>
+        </Header>
       </OnlyOnPrint>
       <Body maxWidth={48} p={3} className="invert">
-        <Toc open>
-          <TocHeading>
-            <Icon glyph="down-caret" size={32} />
-            <Text.span
-              bold
-              caps
-              color="slate"
-              fontSize={2}
-              ml={1}
-              children="Table of Contents"
-            />
-          </TocHeading>
-          <TocList>
-            {tail(headings).map(heading => (
-              <TocItem
-                key={heading.value}
-                pl={theme.space[(heading.depth - 2) * 2]}
-              >
-                <A
-                  href={`#${slugger.slug(heading.value)}`}
-                  children={heading.value}
-                />
-              </TocItem>
-            ))}
-          </TocList>
-        </Toc>
+        {!isEmpty(tail(headings)) && (
+          <Toc open>
+            <TocHeading>
+              <Icon glyph="down-caret" size={32} />
+              <Text.span
+                bold
+                caps
+                color="slate"
+                fontSize={2}
+                ml={1}
+                children="Table of Contents"
+              />
+            </TocHeading>
+            <TocList>
+              {tail(headings).map(heading => (
+                <TocItem
+                  key={heading.value}
+                  pl={theme.space[(heading.depth - 2) * 2]}
+                >
+                  <A
+                    href={`#${slugger.slug(heading.value)}`}
+                    children={heading.value}
+                  />
+                </TocItem>
+              ))}
+            </TocList>
+          </Toc>
+        )}
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </Body>
       <CardsSection py={4}>
@@ -435,7 +372,6 @@ export const pageQuery = graphql`
       html
       fields {
         slug
-        bg
       }
       headings {
         depth

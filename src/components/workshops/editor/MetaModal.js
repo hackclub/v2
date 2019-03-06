5d26891fdc6b64936931e56f274dfbf2c40cd1b5
Lachlan Campbell <lachlan@hackclub.com>
2019-03-06 13:31:01 -0800

    Reduce indentation on MetaModal

--- src/components/workshops/editor/MetaModal.js
+++ src/components/workshops/editor/MetaModal.js
@@ -22,85 +22,82 @@ const NiceField = styled(Field).attrs({
   }
 `
 
-export default ({ active, toggleModal, name, description }) => (
-  <Fragment>
-    {active && (
-      <Fragment>
-        <Modal align="left" my={4} p={[3, 4]}>
-          <Formik
-            initialValues={{ name, description }}
-            validate={values => {
-              let errors = {}
-              if (!values.name) {
-                errors.name = 'Required'
-              }
-              if (!values.description) {
-                errors.description = 'Required'
-              }
-              return errors
-            }}
-            onSubmit={values => {
-              const slug = 'draft-' + values.name.replace(/ /g, '-')
-              const oldSlug = 'draft-' + name.replace(/ /g, '-')
-              const { body, edited } = storage.get(oldSlug)
+export default ({ active, toggleModal, name, description }) =>
+  active ? (
+    <Fragment>
+      <Modal align="left" my={4} p={[3, 4]}>
+        <Formik
+          initialValues={{ name, description }}
+          validate={values => {
+            let errors = {}
+            if (!values.name) {
+              errors.name = 'Required'
+            }
+            if (!values.description) {
+              errors.description = 'Required'
+            }
+            return errors
+          }}
+          onSubmit={values => {
+            const slug = 'draft-' + values.name.replace(/ /g, '-')
+            const oldSlug = 'draft-' + name.replace(/ /g, '-')
+            const { body, edited } = storage.get(oldSlug)
 
-              storage.set(slug, {
-                body,
-                edited,
-                description: values.description
-              })
-              window.location.reload()
-              if (slug !== oldSlug) {
-                storage.remove(oldSlug)
-                window.location = `/workshops/submit?id=${slug}`
-              }
-              toggleModal()
-            }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting
-              /* and other goodies */
-            }) => (
-              <form onSubmit={handleSubmit}>
-                <NiceField
-                  type="input"
-                  name="name"
-                  label="Workshop name"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.name}
-                  error={errors.name && touched.name && errors.name}
-                />
+            storage.set(slug, {
+              body,
+              edited,
+              description: values.description
+            })
+            window.location.reload()
+            if (slug !== oldSlug) {
+              storage.remove(oldSlug)
+              window.location = `/workshops/submit?id=${slug}`
+            }
+            toggleModal()
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <NiceField
+                type="input"
+                name="name"
+                label="Workshop name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+                error={errors.name && touched.name && errors.name}
+              />
 
-                <NiceField
-                  type="input"
-                  name="description"
-                  label="Workshop description"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.description}
-                  error={
-                    errors.description &&
-                    touched.description &&
-                    errors.description
-                  }
-                />
+              <NiceField
+                type="input"
+                name="description"
+                label="Workshop description"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.description}
+                error={
+                  errors.description &&
+                  touched.description &&
+                  errors.description
+                }
+              />
 
-                <Button mt={3} bg="success" onClick={handleSubmit}>
-                  Save
-                </Button>
-              </form>
-            )}
-          </Formik>
-        </Modal>
-        <Overlay onClick={toggleModal} />
-      </Fragment>
-    )}
-  </Fragment>
-)
+              <Button mt={3} bg="success" onClick={handleSubmit}>
+                Save
+              </Button>
+            </form>
+          )}
+        </Formik>
+      </Modal>
+      <Overlay onClick={toggleModal} />
+    </Fragment>
+  ) : null

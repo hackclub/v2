558446f105b04b0d5709f1933127bd51a9968de0
merelinguist <merelinguist@users.noreply.github.com>
2019-03-10 23:50:01 +0000

    Refactor Stats

--- src/components/bank/Stats.js
+++ src/components/bank/Stats.js
@@ -1,50 +1,31 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Stat from 'components/Stat'
 import api from 'api'
 
-class Stats extends Component {
-  state = {}
+import { useInterval } from 'hooks'
 
-  componentDidMount() {
-    this.loadStats()
-    const intervalId = setInterval(this.loadStats, 8000)
-    this.setState({ intervalId })
-  }
+export default props => {
+  const [volume, setVolume] = useState(0)
 
-  componentWillUnmount() {
-    clearInterval(this.state.intervalId)
-  }
+  useEffect(() => {
+    loadStats()
+  })
 
-  loadStats = () => {
+  useInterval(() => {
+    loadStats()
+  }, 8000)
+
+  const loadStats = () => {
     api.get('https://bank.hackclub.com/stats').then(stats => {
-      const { transactions_count, events_count } = stats
       const transactions_volume = Math.floor(stats.transactions_volume / 100)
         .toLocaleString('en-US', {
           style: 'currency',
           currency: 'USD'
         })
         .replace('.00', '')
-      this.setState({
-        transactions_volume,
-        events_count,
-        transactions_count
-      })
+      setVolume(transactions_volume)
     })
   }
 
-  render() {
-    const { state, props } = this
-    const stats = {
-      transactions_volume: 'total transactions to date'
-      // events_count: 'events',
-      // transactions_count: 'transactions'
-    }
-    return Object.keys(state).length > 1
-      ? Object.keys(stats).map(key => (
-          <Stat {...props} value={state[key]} label={stats[key]} key={key} />
-        ))
-      : null
-  }
+  return <Stat {...props} value={volume} label="total amount transacted" />
 }
-
-export default Stats

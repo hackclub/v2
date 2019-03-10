376f1fc4b6cdd8e48a4e54add2753b6fd9d7c686
merelinguist <merelinguist@users.noreply.github.com>
2019-03-10 23:05:51 +0000

    Refactor Help modal

--- src/components/challenge/Help.js
+++ src/components/challenge/Help.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react'
+import React, { useState } from 'react'
 import { Heading, Text, Link } from '@hackclub/design-system'
 import IconButton from 'components/IconButton'
 import { Modal, Overlay, CloseButton } from 'components/Modal'
@@ -16,47 +16,39 @@ const RulesButton = props => (
   />
 )
 
-class Help extends Component {
-  state = { active: false }
+export default () => {
+  const [active, setActive] = useState(false)
 
-  toggleRules = () => {
-    this.setState(state => ({ active: !state.active }))
-  }
+  const toggleRules = () => setActive(!active)
 
-  // Render a modal for challenge rules on button press
-  render() {
-    if (this.state.active) {
-      return (
-        <Fragment>
-          <RulesButton onClick={this.toggleRules} />
-          <Modal align="left" my={4} p={[3, 4]}>
-            <CloseButton onClick={this.toggleRules} />
-            <Heading.h2>Challenge Rules</Heading.h2>
-            <Text fontSize={2} my={3}>
-              Challenge is open to Hack Club community members. It strictly
-              follows Hack Club’s{' '}
-              <Link href="https://conduct.hackclub.com" target="_blank">
-                Code of Conduct
-              </Link>
-              . Anything breaking our Code of Conduct (ex. voter fraud) and
-              contestants found cheating can be temporarily or permanently
-              banned.
-            </Text>
-            <Text fontSize={2}>
-              If you think anyone has violated our Conduct or cheating policy,
-              please reach out to us confidentially at{' '}
-              <Link href="mailto:challenge@hackclub.com">
-                challenge@hackclub.com
-              </Link>
-              .
-            </Text>
-          </Modal>
-          <Overlay onClick={this.toggleRules} />
-        </Fragment>
-      )
-    }
-    return <RulesButton onClick={this.toggleRules} />
+  if (active) {
+    return (
+      <>
+        <RulesButton onClick={toggleRules} />
+        <Modal align="left" my={4} p={[3, 4]}>
+          <CloseButton onClick={toggleRules} />
+          <Heading.h2>Challenge Rules</Heading.h2>
+          <Text fontSize={2} my={3}>
+            Challenge is open to Hack Club community members. It strictly
+            follows Hack Club’s{' '}
+            <Link href="https://conduct.hackclub.com" target="_blank">
+              Code of Conduct
+            </Link>
+            . Anything breaking our Code of Conduct (ex. voter fraud) and
+            contestants found cheating can be temporarily or permanently banned.
+          </Text>
+          <Text fontSize={2}>
+            If you think anyone has violated our Conduct or cheating policy,
+            please reach out to us confidentially at{' '}
+            <Link href="mailto:challenge@hackclub.com">
+              challenge@hackclub.com
+            </Link>
+            .
+          </Text>
+        </Modal>
+        <Overlay onClick={toggleRules} />
+      </>
+    )
   }
+  return <RulesButton onClick={toggleRules} />
 }
-
-export default Help

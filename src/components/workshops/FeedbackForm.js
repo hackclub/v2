dd04356c03e57301478b913b1defd920fed01ead
merelinguist <merelinguist@users.noreply.github.com>
2019-03-10 12:42:24 +0000

    Add workshops

---
+++ src/components/workshops/FeedbackForm.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Field, Submit } from 'components/Forms'
+import { withFormik } from 'formik'
+import { map, last, keys, omit } from 'lodash'
+import * as yup from 'yup'
+import api from 'api'
+import { workshopFeedback } from 'data.json'
+
+const statusMessage = status =>
+  status
+    ? {
+        success: 'Sent, thank you!',
+        error: 'Something went wrong 🚨'
+      }[status]
+    : 'Submit your feedback'
+const statusColor = status =>
+  status === 'success' || status === 'error' ? status.toString() : 'primary'
+const InnerForm = ({
+  values,
+  errors,
+  touched,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+  status
+}) => (
+  <form onSubmit={handleSubmit}>
+    {map(workshopFeedback, (question, key) => (
+      <Field
+        label={question}
+        name={key}
+        type="textarea"
+        rows={2}
+        value={values[key]}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched[key] && errors[key]}
+        bg="white"
+        key={key}
+      />
+    ))}
+    <Submit
+      disabled={isSubmitting}
+      onClick={handleSubmit}
+      value={statusMessage(status)}
+      bg={statusColor(status)}
+      fontSize={2}
+      mt={2}
+    />
+  </form>
+)
+const schema = map(keys(workshopFeedback), key => ({
+  [key]: yup.string().required('required')
+}))
+const FeedbackForm = withFormik({
+  validationSchema: yup.object().shape(schema),
+  enableReinitialize: true,
+  handleSubmit: (data, { props, setStatus, setSubmitting, resetForm }) => {
+    const feedback = {}
+    map(omit(data, 'slug'), (res, id) => {
+      // eslint-disable-next-line
+      feedback[questions[id]] = res
+    })
+    const body = JSON.stringify({
+      workshop_slug: last(props.slug.split('/')),
+      feedback
+    })
+    api
+      .post(`v1/workshop_feedbacks`, {
+        headers: { 'Content-Type': 'application/json' },
+        body
+      })
+      .then(res => {
+        resetForm()
+        setStatus('success')
+      })
+      .catch(e => {
+        setSubmitting(false)
+        setStatus('error')
+      })
+  },
+  displayName: 'FeedbackForm'
+})(InnerForm)
+export default FeedbackForm
---
+++ src/components/workshops/WorkshopSearch.js
@@ -0,0 +1,81 @@
+import React, { Component, Fragment } from 'react'
+import { Container } from '@hackclub/design-system'
+import SearchInput from './SearchInput'
+import Track from './Track'
+import NoResults from './NoResults'
+import Fuse from 'fuse.js'
+import { groupBy, toPairs } from 'lodash'
+
+const groupOrder = [
+  'start',
+  'club',
+  'challenges',
+  'pi',
+  'react',
+  'arduino',
+  'experimental',
+  'misc',
+  'retired'
+]
+const keys = ['node.frontmatter.name', 'node.frontmatter.description']
+
+class WorkshopSearch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.fuse = new Fuse(props.workshops, { threshold: 0.4, keys })
+  }
+
+  handleInputChange = e => this.setState({ value: e.target.value })
+
+  render() {
+    const results =
+      this.state.value === ''
+        ? this.props.workshops
+        : this.fuse.search(this.state.value)
+    const groups = groupBy(results, 'node.frontmatter.group')
+
+    // sort groups based on groupOrder
+    const sortedGroups = toPairs(groups).sort((a, b) => {
+      // if a group isn't found in groupOrder, ensure it appears last in the
+      // sorted list
+      if (groupOrder.indexOf(a[0]) === -1) {
+        return 1
+      } else if (groupOrder.indexOf(b[0]) === -1) {
+        return -1
+      }
+
+      return groupOrder.indexOf(a[0]) - groupOrder.indexOf(b[0])
+    })
+
+    return (
+      <Fragment>
+        <Container maxWidth={42} mt={-4} px={3} style={{ zIndex: 2 }}>
+          <SearchInput
+            placeholder="Search workshops"
+            label="Search"
+            value={this.state.value}
+            onChange={this.handleInputChange}
+          />
+        </Container>
+        {results.length !== 0 ? (
+          <Fragment>
+            {sortedGroups.map(group => (
+              <Track
+                key={`workshops-${group[0]}`}
+                name={group[0]}
+                data={group[1]}
+              />
+            ))}
+          </Fragment>
+        ) : (
+          <NoResults value={this.state.value} />
+        )}
+      </Fragment>
+    )
+  }
+}
+
+export default WorkshopSearch

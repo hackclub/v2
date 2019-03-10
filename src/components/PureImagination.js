bc020358bc06a310720205e758114c87329a2c48
merelinguist <merelinguist@users.noreply.github.com>
2019-03-10 23:52:55 +0000

    Refactor Imagination

--- src/components/PureImagination.js
+++ src/components/PureImagination.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default class extends Component {
-  codeword = 'pi'
+export default () => {
+  const codeword = 'pi'
 
-  state = { progress: 0 }
+  const [progress, setProgress] = useState(0)
 
-  componentDidMount() {
+  useEffect(() => {
     window.document.onkeypress = e => {
-      const shouldProgress = e.key === this.codeword[this.state.progress]
-      this.setState(state => ({
-        progress: shouldProgress ? state.progress + 1 : 0
-      }))
+      const shouldProgress = e.key === codeword[this.state.progress]
+      setProgress(shouldProgress ? progress + 1 : 0)
     }
-  }
+  })
 
-  render() {
-    return this.state.progress === this.codeword.length ? (
-      <iframe
-        title="pi"
-        width={560}
-        height={315}
-        src="https://www.youtube.com/embed/knIfoQW_mZg?autoplay=1"
-        frameborder={0}
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        style={{ display: 'none' }}
-      />
-    ) : null
-  }
+  return this.state.progress === this.codeword.length ? (
+    <iframe
+      title="pi"
+      width={560}
+      height={315}
+      src="https://www.youtube.com/embed/knIfoQW_mZg?autoplay=1"
+      frameborder={0}
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      style={{ display: 'none' }}
+    />
+  ) : null
 }

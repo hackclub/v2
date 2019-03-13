81dfad42ebff2aadba3146f6311c1c3823dda4e6
Lachlan Campbell <lachlan@hackclub.com>
2019-03-13 13:50:15 -0700

    Remove unused imports

--- src/pages/redeem_tech_domain.js
+++ src/pages/redeem_tech_domain.js
@@ -1,6 +1,5 @@
-import styled from 'styled-components'
 import React from 'react'
-import { Container, Section, theme } from '@hackclub/design-system'
+import { Container, Section } from '@hackclub/design-system'
 import Layout from 'components/Layout'
 import Helmet from 'react-helmet'
 import { Headline, Lead } from 'components/Content'
@@ -8,17 +7,12 @@ import Nav from 'components/Nav'
 import Sheet from 'components/Sheet'
 import RedeemTechDomainForm from 'components/RedeemTechDomainForm'
 
-const Base = styled(Section).attrs({
-  px: 3,
-  align: 'center'
-})``
-
 export default () => (
   <Layout bg="snow">
     <Helmet title="Free .TECH Domain – Hack Club" />
     <Nav color="slate" />
-    <Base>
-      <Container color="black" maxWidth={32} pt={[5, 6]}>
+    <Section>
+      <Container color="black" maxWidth={32} pt={[5, 6]} px={3} align="center">
         <Headline color="primary" mb={3}>
           Free .TECH Domain!
         </Headline>
@@ -34,6 +28,6 @@ export default () => (
       <Sheet maxWidth={24} align="left" my={4} mx="auto">
         <RedeemTechDomainForm />
       </Sheet>
-    </Base>
+    </Section>
   </Layout>
 )

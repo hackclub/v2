ccc337513f03ee2a0a0bd5b6baf5cbfd5e5c5322
Lachlan Campbell <lachlan@hackclub.com>
2019-03-13 13:48:05 -0700

    Redesign .TECH page

--- src/pages/redeem_tech_domain.js
+++ src/pages/redeem_tech_domain.js
@@ -10,36 +10,28 @@ import RedeemTechDomainForm from 'components/RedeemTechDomainForm'
 
 const Base = styled(Section).attrs({
   px: 3,
-  align: 'center',
-  color: 'white'
-})`
-  background-color: ${theme.colors.red[5]};
-  background-image: linear-gradient(
-    -32deg,
-    ${theme.colors.orange[4]},
-    ${theme.colors.red[5]},
-    ${theme.colors.red[6]}
-  );
-  min-height: 100vh;
-`
+  align: 'center'
+})``
 
 export default () => (
-  <Layout>
+  <Layout bg="snow">
     <Helmet title="Free .TECH Domain – Hack Club" />
-    <Nav />
+    <Nav color="slate" />
     <Base>
-      <Container maxWidth={32} pt={[5, 6]}>
-        <Headline mb={3}>Free .TECH Domain!</Headline>
-        <Lead color="red.0" maxWidth={28} mb={2}>
-          Every Hack Club member gets a free .TECH domain from our awesome
-          partners.
+      <Container color="black" maxWidth={32} pt={[5, 6]}>
+        <Headline color="primary" mb={3}>
+          Free .TECH Domain!
+        </Headline>
+        <Lead maxWidth={28} mb={2}>
+          Every Hack Club member gets a free domain from the awesome folks at
+          .TECH.
         </Lead>
-        <Lead color="red.0" maxWidth={28}>
-          Submit your request below and the .TECH team will set you up—it
-          generally takes about a day.
+        <Lead maxWidth={28}>
+          Request a domain below & they’ll set you up. It generally takes about
+          a day.
         </Lead>
       </Container>
-      <Sheet maxWidth={28} align="left" my={4} mx="auto">
+      <Sheet maxWidth={24} align="left" my={4} mx="auto">
         <RedeemTechDomainForm />
       </Sheet>
     </Base>

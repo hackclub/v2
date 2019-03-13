5c9b5ea6a55e4fdd70fce1724defad13a620e336
Lachlan Campbell <lachlan@hackclub.com>
2019-03-13 14:23:23 -0700

    Remove admin + checkup

--- src/components/admin/LeaderProfileInfo.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import LeaderApplicationForm from 'components/apply/LeaderApplicationForm'
-
-export default props => (
-  <LeaderApplicationForm params={props.leaderProfile} disableAutosave />
-)

af92567800aa7f6c5294f2374a2996da4246911d
Lachlan Campbell <lachlan@hackclub.com>
2019-03-13 15:49:01 -0700

    Fix ErrorPage error

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Text } from '@hackclub/design-system'
 
+const alarm = (
+  <span role="img" aria-label="alert emoji">
+    🚨
+  </span>
+)
+
 const ErrorPage = ({
   message = 'Something terrible has happened',
   ...props
 }) => (
   <Text color="error" py={3} align="center" {...props}>
-    🚨 {message} 🚨
+    {alarm} {message} {alarm}
     <br />
     Please let us know about this by emailing us at{' '}
     <a href="mailto:bug@hackclub.com">bug@hackclub.com</a>

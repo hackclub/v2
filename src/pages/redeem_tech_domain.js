2dca08015b12bf5bb6104395447ac900aad6a790
Lachlan Campbell <lachlan@hackclub.com>
2019-03-13 17:27:07 -0700

    Deprecate .TECH form

--- src/pages/redeem_tech_domain.js
+++ src/pages/redeem_tech_domain.js
@@ -1,33 +1,42 @@
 import React from 'react'
-import { Container, Section } from '@hackclub/design-system'
+import styled from 'styled-components'
+import {
+  Container,
+  Icon,
+  LargeButton as Button,
+  Link as A,
+  Section,
+  Text,
+  theme
+} from '@hackclub/design-system'
 import Layout from 'components/Layout'
 import Helmet from 'react-helmet'
-import { Headline, Lead } from 'components/Content'
+import { Link } from 'gatsby'
 import Nav from 'components/Nav'
-import Sheet from 'components/Sheet'
-import RedeemTechDomainForm from 'components/RedeemTechDomainForm'
+import Footer from 'components/Footer'
+import { Headline, Lead } from 'components/Content'
+
+const Header = styled(Section)`
+  background-color: ${theme.colors.primary};
+  background-image: ${theme.gradient('cyan.6', 'teal.6')};
+`
+
+A.link = A.withComponent(Link)
 
 export default () => (
-  <Layout bg="snow">
-    <Helmet title="Free .TECH Domain – Hack Club" />
-    <Nav color="slate" />
-    <Section>
-      <Container color="black" maxWidth={32} pt={[5, 6]} px={3} align="center">
-        <Headline color="primary" mb={3}>
-          Free .TECH Domain!
-        </Headline>
-        <Lead maxWidth={28} mb={2}>
-          Every Hack Club member gets a free domain from the awesome folks at
-          .TECH.
-        </Lead>
-        <Lead maxWidth={28}>
-          Request a domain below & they’ll set you up. It generally takes about
-          a day.
-        </Lead>
+  <Layout>
+    <Helmet title=".TECH domains – Hack Club" />
+    <Nav />
+    <Header align="center" color="white" px={0}>
+      <Container maxWidth={32} p={3} mt={[5, 6]}>
+        <Icon glyph="history" size={64} />
+        <Headline mb={0}>Our .TECH domain offer expired.</Headline>
       </Container>
-      <Sheet maxWidth={24} align="left" my={4} mx="auto">
-        <RedeemTechDomainForm />
-      </Sheet>
-    </Section>
+      <Lead maxWidth={32} px={3} mb={[4, 5]} fontSize={3}>
+        We used to have a partnership with .TECH domains to give Hack Clubbers
+        free domains. Sadly, that offer is no longer available.
+      </Lead>
+    </Header>
+    <Footer />
   </Layout>
 )

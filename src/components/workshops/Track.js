35449bcd9545f8f40d2ff28cb38d3c9aa6d5c39d
Max Wofford <max@maxwofford.com>
2019-03-27 11:42:16 -0700

    Quick fix for broken workshop link b/c board meeting is happening today

--- src/components/workshops/Track.js
+++ src/components/workshops/Track.js
@@ -85,7 +85,7 @@ const WorkshopItem = ({
   },
   ...props
 }) => (
-  <Link to={slug} {...props}>
+  <Link to={`https://5c8804a629a378000833619c--hackclub.netlify.com/workshops/${slug}`} {...props}>
     <Item style={{ backgroundImage: `url('${bg}')` }}>
       <Heading.h3 color="white" fontSize={3} mb={[null, 3]} children={name} />
       <Text color="snow" fontSize={2} children={description} />

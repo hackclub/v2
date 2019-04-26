85e1c31b7ad171f6202721c5285dd37f17b8a033
Lachlan Campbell <lachlan@hackclub.com>
2019-04-26 09:26:14 -0400

    Iterate Activities

--- src/pages/camp/Activities.js
+++ src/pages/camp/Activities.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
-import { BackgroundImage, Container, theme } from '@hackclub/design-system'
+import {
+  BackgroundImage,
+  Flex,
+  Container,
+  theme
+} from '@hackclub/design-system'
 import { Section, SectionTitle, Cols } from './Content'
-import { Headline, Subhline, Featline, Title, Lead } from 'components/Content'
+import { Headline, Subhline, Highlight, Title, Lead } from 'components/Content'
 import Photo from 'components/Photo'
 
 const FullBleed = styled(BackgroundImage)`
-  // position: absolute;
+  position: relative;
   width: 100%;
   min-height: 24rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   text-shadow: 0 1px 4px rgba(0, 0, 0, 0.75);
+`
+const FullBleedCaption = styled(Flex).attrs({
+  fontSize: 1,
+  py: 1,
+  px: 3,
+  m: [3, 4],
+  align: 'center',
+  color: 'dark'
+})`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  border-radius: ${theme.pill};
+  background: rgba(255, 255, 255, 0.875);
+  text-shadow: none;
+  line-height: 1.75;
+  @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
+    background: rgba(255, 255, 255, 0.75) !important;
+    -webkit-backdrop-filter: saturate(180%) blur(2px);
+  }
+  ${theme.mediaQueries.reduceTransparency} {
+    background: ${theme.colors.muted} !important;
+  }
 `
 
 const Gallery = styled(Container).attrs({ maxWidth: 84, px: [3, 4] })`
@@ -41,18 +69,6 @@ export default () => (
           “instructional” sessions are all led by leaders.
         </Lead>
       </Cols>
-      <Cols cols="1fr 3fr" mt={[4, 5, 6]}>
-        <Headline pr={3}>Safety.</Headline>
-        <Lead pl={[3, 0]}>
-          In the weeks leading up to Camp, we’ll work one-on-one with every
-          attendee to prepare a 2-hour workshop about something technical they
-          love. They’ll share these at Camp, one every morning—so the main
-          “instructional” sessions are all led by leaders, for leaders.
-        </Lead>
-      </Cols>
-    </Container>
-    <Container maxWidth={48} px={3}>
-      <Lead mb={4} />
     </Container>
     <FullBleed
       src="/camp/dolores.jpg"
@@ -62,28 +78,31 @@ export default () => (
       color="white"
       px={3}
       pb={[4, null, 6]}
+      my={[4, 5, 6]}
     >
       <Subhline mb={0}>Welcome to</Subhline>
-      <Title>San&nbsp;Francisco.</Title>
+      <Title mb={0}>San&nbsp;Francisco.</Title>
+      <FullBleedCaption>Mission Dolores Park</FullBleedCaption>
     </FullBleed>
     <Container maxWidth={48} px={3}>
-      <Lead>
+      <Lead mb={4}>
+        Hack Camp takes place entirely in San Francisco. You’ll live with your
+        fellow campers at Home Base, a house in the Sunset. During the day,
+        you’ll be at HQ, our office in SoMA.
+      </Lead>
+      <Lead mb={4}>
         Every day, new challenges will await campers, encompassing everything
         from adulting & high-level engineering to safe-cracking & sewing. You’ll
         have to leverage your diverse skill sets and communication skills as you
-        learn to be independent. Expect the unexpected.
-      </Lead>
-      <Lead mb={4}>
-        We’ll have speakers every other day. Robert from Radiolab May-Li from
-        Khan Academy Tom from GitHub
+        learn to be independent. <Highlight>Expect the unexpected.</Highlight>
       </Lead>
       <Lead>
         On weekends, we’ll go on adventures, like touring the Facebook campus in
         Menlo Park, taking a trip to Angel Island outside San Francisco, and
-        visiting the Exploratorium science museum.
+        visiting the Exploratorium museum.
       </Lead>
     </Container>
-    <Gallery mt={[4, 5, 6]}>
+    <Gallery my={[4, 5, 6]}>
       <Photo
         src="/camp/street.jpg"
         alt="Natoma St, HQ on the left"
@@ -101,5 +120,11 @@ export default () => (
       <Photo src="/camp/conference.jpg" alt="HQ’s conference room" />
       <Photo src="/camp/library.jpg" alt="HQ’s library" />
     </Gallery>
+    <Container maxWidth={48} px={3}>
+      <Lead mb={4}>
+        We’ll have speakers every other day. Robert from Radiolab May-Li from
+        Khan Academy Tom from GitHub
+      </Lead>
+    </Container>
   </Section>
 )
--- src/pages/camp/Content.js
+++ src/pages/camp/Content.js
@@ -4,7 +4,7 @@ import { Title, Headline, Lead } from 'components/Content'
 
 export const Section = styled(Box.section).attrs({ py: [5, 6, 7, 8] })``
 
-export const SectionTitle = styled(Title).attrs({ fontSize: [7, 8, 9, 10] })``
+export const SectionTitle = styled(Title).attrs({ fontSize: [7, 8, 9, 128] })``
 
 export const Cols = styled(Box)`
   display: grid;

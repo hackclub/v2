ba188c832fc480c5b9e37cd470268e1e7350f078
Lachlan Campbell <lachlan@hackclub.com>
2019-04-30 20:01:17 -0400

    Add social card to Camp page

--- src/pages/camp/index.js
+++ src/pages/camp/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 import Layout from 'components/Layout'
 import Footer from 'components/Footer'
 
@@ -10,24 +9,18 @@ import Manifesto from './Manifesto'
 import Activities from './Activities'
 import Admissions from './Admissions'
 
-const description =
-  'Learn how to start a coding club at your high school through Hack Club. Get programming club ideas, curriculum, activities, and more.'
 const title = 'Hack Camp – Summer 2019 High School Coding Camp'
+const desc =
+  'July 2019, Hack Club’s high school learn-to-code summer camp returns to San Francisco.'
 
 export default () => (
-  <Layout bg="dark">
-    <Helmet
-      title={title}
-      meta={[
-        { name: 'twitter:title', content: title },
-        { name: 'description', content: description },
-        { name: 'twitter:description', content: description },
-        { property: 'og:title', content: title },
-        { property: 'og:description', content: description },
-        { property: 'og:url', content: 'https://hackclub.com/camp' }
-      ]}
-    />
-    {/* <Nav color="slate" unfixed /> */}
+  <Layout
+    bg="dark"
+    title={title}
+    desc={desc}
+    img="/cards/camp.png"
+    path="/camp/"
+  >
     <PageNav />
     <style>{`
       body {

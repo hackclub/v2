0c9ca048ce56333c818e1b386a70c9cd50b05f85
Max Wofford <max@maxwofford.com>
2020-04-01 21:15:34 -0400

    Replace with Mingji's link

--- src/pages/index.js
+++ src/pages/index.js
@@ -365,7 +365,7 @@ const OnlyDesktop = styled.span`
 `
 
 export default () => (
-  <iframe src="https://maxwofford.com" width="100vw" style={{
+  <iframe src="https://best-veiled-need.glitch.me/" width="100vw" style={{
 position:'fixed', top:0, left:0, bottom:0, right:0, width:'100%', height: '100%', border:'none', margin:0, padding:0, overflow:'hidden'
   }}></iframe>
 )

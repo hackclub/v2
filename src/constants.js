368a00e35c48038fc78d8dbf97e499c4c961d903
Lachlan Campbell <lachlan@hackclub.com>
2020-04-27 16:30:09 -0400

    Delete constants.js

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export const IS_PROD = process.env.NODE_ENV === 'production'
-
-export const PUBLIC_STRIPE_KEY = IS_PROD
-  ? 'pk_live_z9sqsmu2zPUSaCYgLmfoHSNm'
-  : 'pk_test_eU1Cn0yK7HTAPZeXjA0uwWL8'

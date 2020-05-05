6065d60136d22dcab7d4efb20b21a2fe414d89ac
Lachlan Campbell <lachlan@hackclub.com>
2020-05-05 17:30:12 -0400

    Begin recent feature list

--- src/components/bank/Everything.js
+++ src/components/bank/Everything.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import {
+  Badge,
   Box,
   Container,
   Link as A,
@@ -11,6 +12,7 @@ import {
 } from '@hackclub/design-system'
 import { Headline, Lead } from 'components/Content'
 import { Fade } from 'react-reveal'
+import { timeSince } from 'helpers'
 
 const Base = styled(Box.section).attrs({
   bg: 'dark',
@@ -53,8 +55,9 @@ const Percentage = styled(Flex).attrs({
 const List = styled(Text.withComponent('ol')).attrs({ pl: 0 })`
   list-style: none;
   ${theme.mediaQueries.md} {
-    column-count: 2;
-    column-gap: ${theme.space[4]}px;
+    display: grid;
+    grid-column-gap: ${theme.space[4]}px;
+    grid-template-columns: repeat(2, 1fr);
   }
   li {
     line-height: 1.25;
@@ -65,7 +68,7 @@ const List = styled(Text.withComponent('ol')).attrs({ pl: 0 })`
   }
 `
 
-List.Item = props => (
+List.Item = (props) => (
   <Fade left>
     <li>
       <Icon glyph="enter" color="muted" size={32} mr={2} />
@@ -82,8 +85,6 @@ export default () => (
       </Container>
       <List>
         {[
-          'Physical debit cards',
-          'G Suite accounts & email addresses',
           'Automated tax filings',
           'Legal entity with 501(c)(3) status',
           'Bank account backed by Silicon Valley Bank',
@@ -91,15 +92,29 @@ export default () => (
           'Collect donations via card, check, or ACH',
           'Real-time dashboard of finances',
           'Share access with your whole team',
-          'Pre-written legal forms for event attendees',
           'Record shared notes on transactions',
           'Transaction data export',
           'Dedicated point of contact',
           '24-hour response support',
           'Negotiated nonprofit rates with Stripe',
           'Reimbursement process'
-        ].map(item => (
+        ].map((item) => (
           <List.Item key={item} children={item} />
+        ))}
+        {Object.entries({
+          'Immediate legal forms for attendees': '2020-01-15',
+          'Online embeddable donation form': '2020-03-10',
+          'Instant check sending & voiding': '2019-09-18',
+          'Online ACH transfers': '2019-09-18',
+          'Instant G Suite & email addresses': '2020-01-15',
+          'Instant virtual debit cards': '2020-03-08',
+          'Card transactions paper trail': '2020-03-10',
+          'Transparency Mode (optional)': '2020-05-05',
+          'Instant, no-contract account setup': '2020-05-05'
+        }).map(([item, date]) => (
+          <List.Item key={item}>
+            <Badge mr={1}>{timeSince(date)}</Badge> {item}
+          </List.Item>
         ))}
       </List>
     </Container>

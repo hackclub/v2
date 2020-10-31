e3ab1a3b31b6fedd61c6042ea8ee6a020d362e64
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2020-10-31 20:33:52 +0530

    NEW CHANGE

--- src/pages/bank.js
+++ src/pages/bank.js
@@ -24,7 +24,7 @@ const styles = `
 
 const title = 'Hack Club Bank – The Bank For Student Hackers'
 const desc =
-  'Hack Club Bank provides a 501(c)(3) status-backed bank account optimized for high school hackathons including invoicing, debit cards, check sending, pre-written legal forms, automated tax filing, and transparent finances. Get fiscal sponsorship designed to help you run great events.'
+  'Hack Club Bank provides a 501(c)(3)status-backed bank account optimized for high school hackathons including invoicing, debit cards, check sending, pre-written legal forms, automated tax filing, and transparent finances. Get fiscal sponsorship designed to help you run great events.'
 
 export default () => (
   <Layout title={title} desc={desc} img="https://cloud-jjiw4lhcn.vercel.app/2020-08-08_edbp3wzddj5rn9nvz5vuqjzm4dezyvr8.jpeg" path="/bank/">
